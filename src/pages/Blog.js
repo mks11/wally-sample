c3f276bba15680315c34f344fead16d066e46614
rizkix <rizki.fuad@gmail.com>
2018-10-07 14:13:09 +0700

    blog items

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Title from '../common/page/Title'
 import { connect } from '../utils'
+import moment from 'moment'
 
 class About extends Component {
   constructor(props, context){
@@ -27,6 +28,22 @@ class About extends Component {
       <div className="App">
         <Title content="Blog" />
         <section>
+          <div className="container mt-5">
+            { this.contentStore.blog.map((item) => (
+              <div className="row blog-item">
+                <div className="col">
+                  <img className="img-fluid" src={item.image_ref} />
+                </div>
+                <div className="col">
+                  <h2 className="m-0 p-0">{item.title}</h2>
+                  <div className="my-3 blog-date">Posted {moment(item.createdAt).format('MMMM DD, YYYY')} by {item.author}</div>
+                  <p dangerouslySetInnerHTML={{__html: item.body}}>
+                  </p>
+                </div>
+                <hr/>
+              </div>
+            ))}
+          </div>
 
         </section>
       </div>
