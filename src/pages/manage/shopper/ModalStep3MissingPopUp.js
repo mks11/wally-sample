3e2c71afd901bf1af55025f8ce451fc15c816254
Diego Yanez <diegoyanez@MacBook-Pro-9.fios-router.home>
2019-06-08 17:39:27 -0400

    remove fake data, add submit functionality, refactor code

--- src/pages/manage/shopper/ModalStep3MissingPopUp.js
+++ src/pages/manage/shopper/ModalStep3MissingPopUp.js
@@ -4,17 +4,16 @@ import { connect } from '../../../utils'
 import {
 Modal,
 ModalBody,
-ModalFooter,
 Button,
 Container,
 Row,
 Col
 } from 'reactstrap'
 import Table from '@material-ui/core/Table'
-import Paper from '@material-ui/core/Paper'
+
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
-import TableHead from '@material-ui/core/TableHead'
+
 import TableRow from '@material-ui/core/TableRow'
 import { Checkbox } from '@material-ui/core';
 import Input from '@material-ui/core/Input'
@@ -23,92 +22,122 @@ class ModalStep3MissingPopUp extends Component {
     constructor(props){
         super(props)
         this.state = {
-            id: "",
-            status: "",
-            quantity: "",
-            selected: []
+            id: null,
+            status: null,
+            quantity: null,
+            selected: null,
+            shopitem: null,
+            timeframe: null
         }
         this.adminStore = this.props.store.admin
     }
 
-   
 
-    //functions i might need to update item
-    // setShopItemStatus(status, shopitem_id)
-    //updateShopItemQuantity(timeframe, shopitem_id, data)
+    componentDidMount(){
+        const {id, status, timeframe} = this.props
+        this.setState({
+            id: id,
+            status: status,
+            timeframe: timeframe
+        })
+    }
 
-    handleOnClick = (e) => {
-        let checkedStatus = e.target.value
+
+    handleOnChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+        console.log(this.state)
+    }
+
+    handleChecked = () => {
+        const { selected } = this.state
+
+        if ( selected === "missing" ){
+            return true
+        } else {
+            return false
+        }
+    }
+
+    handleSubmit = async() =>{
+        //debugger
+        const {toggleModal, id} = this.props
+        const {selected, quantity, timeframe} = this.state
         let status = ""
-        
-        if ( checkedStatus === "ugly" || checkedStatus === "too little"){
+
+        if (selected === "ugly" || selected === "too little") {
             status = "issue"
         } else {
             status = "missing"
-
         }
 
-        this.setState({
-            status: status
-        })
-        console.log(status)
-        console.log(this.state.status)
-    }
 
-    handleOnChange = () => {
+        // uncomment when ready for testing against API
+        // await this.adminStore.setShopItemStatus(status, id)
+        // await this.adminStore.updateShopItemQuantity(timeframe, id, quantity)
 
-    }
-
-    handleSubmit = () =>{
-
+        console.log(id, status, quantity)
+        toggleModal()
     }
 
     render(){
-        
         const { showModal, toggleModal } = this.props 
-
-
+        let renderQuantity 
+        if( this.props.shopitem ){
+            renderQuantity =    <TableCell 
+                                 align = "center">
+                                    {this.props.shopitem.quantity } { this.props.shopitem.unit_type === "packaging" ? this.props.shopitem.packaging_name : this.props.shopitem.unit_type }
+                                </TableCell>
+        } else {
+            renderQuantity = null
+        }
+        
         return(
-            <Modal isOpen = { false } toggle = { toggleModal }>
+            <Modal isOpen = { showModal } toggle = { toggleModal }>
                 <ModalBody>
                     <Container>
                         
-                            <h3>  {this.props.shopitem.product_name} Unavailable</h3>
+                            <h3>  {this.props.shopitem ? this.props.shopitem.product_name : null} Unavailable</h3>
                             <Table >
 
                                 <TableBody>
-                                    <h5>Missing Reason:</h5>
+                                    <h5>Missing Reason (please select one):</h5>
                                     <TableRow>
                                         <TableCell> Missing :</TableCell>
                                         <TableCell align = "center">
-                                            <Checkbox value = "missing" onClick={ (e) => this.handleOnClick(e) }/>
+                                            <Checkbox   id = "selected" value = "missing" onClick={ this.handleOnChange }/>
                                         </TableCell>
                                     </TableRow>
                                     <TableRow >
                                         <TableCell > Ugly:</TableCell>
                                         <TableCell align = "center">
-                                            <Checkbox value = "ugly" onClick = { (e) => this.handleOnClick(e) }/>
+                                            <Checkbox value = "ugly" id = "selected" onClick = { this.handleOnChange }/>
                                         </TableCell>
                                     </TableRow>
                                     <TableRow >
                                         <TableCell > Too little:</TableCell>
                                         <TableCell align = "center">
-                                            <Checkbox value = "too little" onClick = { (e) => this.handleOnClick(e) }/>
+                                            <Checkbox value = "too little" id = "selected" onClick = { this.handleOnChange }/>
                                         </TableCell>
                                     </TableRow>
                                     <TableRow >
                                         <TableCell > 
-                                        <strong>Quantity Available:</strong>
+                                        <strong>Quantity Available:</strong><br/>
+                                        (if ugly or too little. how many available?)
                                         </TableCell>
                                         <TableCell >
-                                        <Input 
-                                        style = {{
-                                            width: "60px"
-                                        }}/>
-                                        </TableCell>
-                                        <TableCell align = "center">
-                                            { this.props.shopitem.quantity } { this.props.shopitem.unit_type === "packaging" ? this.props.shopitem.packaging_name : this.props.shopitem.unit_type }
-                                        </TableCell>
+                                        
+                                       <Input
+                                            onChange = {this.handleOnChange}
+                                            value = {this.state.quantity}
+                                            id = "quantity"
+                                            style = {{
+                                                width: "60px"
+                                            }}/>
+                                        
+                                       </TableCell>
+                                       {renderQuantity} 
                                     </TableRow>
                                 </TableBody>
                                         
@@ -117,6 +146,7 @@ class ModalStep3MissingPopUp extends Component {
                          <Row>
                          <Col  style = {{paddingTop: "20px"}} sm="12" md={{ size: 6, offset: 4 }}>
                          <Button 
+                            value = {this.props.shopitem ? this.props.shopitem._id : null}
                             className = "btn-sm"
                             onClick = {(e) => this.handleSubmit(e)}> Submit </Button>
                         </Col>
