1b3909378f2d9bcea442b299687f01f0fab57c4f
Theresa Morelli <theresa.l.morelli@gmail.com>
2019-06-14 14:29:32 -0400

    Remove test data

--- src/pages/manage/ShopperTable.js
+++ src/pages/manage/ShopperTable.js
@@ -52,21 +52,10 @@ class ShopperTable extends Component {
   }
 
   render() {
-    let {
-      shopitemsFarms,
-    } = this.adminStore
+    let {shopitemsFarms} = this.adminStore
     const {shopitems} = this.props
     const {timeframe} = this.props
     const totalPrice = ({shopitems}) => shopitems && shopitems.reduce((sum, item) => sum + item.estimated_total, 0)
-    // const renderStatus = (shopitem) => {
-    //   if (shopitem.completed) {
-    //     return 'Completed'
-    //   } else if (shopitem.missing) {
-    //     return 'Missing'
-    //   } else {
-    //     return 'Incomplete'
-    //   }
-    // }
     
     return (
       <Paper elevation={1} className={"scrollable-table"}>
@@ -88,7 +77,6 @@ class ShopperTable extends Component {
               return (
                 <TableRow
                   key={shopitem.inventory_id}
-                  // className={`row ${renderStatus(shopitem).toLocaleLowerCase()} `}
                   className={`row ${shopitem.status} `}
                   onClick={() => this.props.toggleSingleProductView(shopitem, i)}
                 >
