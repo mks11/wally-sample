4464d0fc73c5436cce33a0ae4c9d17aeddf4a663
kirbycampbell <jkirbycampbell@gmail.com>
2019-09-22 18:38:25 -0700

    Updated Return Location

--- src/pages/Checkout/Returns/index.js
+++ src/pages/Checkout/Returns/index.js
@@ -1,82 +1,82 @@
-import React, { Component } from "react";
-import ClickOutside from "react-click-outside";
+import React, {Component} from 'react';
+import ClickOutside from 'react-click-outside';
 
 class Returns extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super (props);
 
     this.state = {
       confirmReturn: false,
       returnDropdown: false,
-      selected: ""
+      selected: '',
     };
   }
 
-  componentDidMount() {
+  componentDidMount () {
     if (this.props.default) {
-      this.setState({
+      this.setState ({
         confirmReturn: true,
-        selected: this.props.default
+        selected: this.props.default,
       });
-      this.props.onReturnChange(this.state.confirmReturn, this.state.selected);
+      this.props.onReturnChange (this.state.confirmReturn, this.state.selected);
     }
   }
 
-  toggleReturnDropdown(e) {
+  toggleReturnDropdown (e) {
     if (this.state.confirmReturn) {
-      this.setState({ returnDropdown: !this.state.returnDropdown });
+      this.setState ({returnDropdown: !this.state.returnDropdown});
     }
   }
 
-  hideReturnDropdown(e) {
+  hideReturnDropdown (e) {
     if (!this.state.returnDropdown) {
       return;
     }
 
-    this.setState({ returnDropdown: false });
+    this.setState ({returnDropdown: false});
   }
 
   handleConfirmReturn = () => {
-    this.setState({
-      confirmReturn: !this.state.confirmReturn
+    this.setState ({
+      confirmReturn: !this.state.confirmReturn,
     });
   };
 
-  handleSelectedReturn(location) {
-    this.setState({
+  handleSelectedReturn (location) {
+    this.setState ({
       selected: location,
-      returnDropdown: false
+      returnDropdown: false,
     });
-    this.props.onReturnChange(this.state.confirmReturn, this.state.selected);
+    this.props.onReturnChange (this.state.confirmReturn, location);
   }
 
-  render() {
-    const { title } = this.props;
+  render () {
+    const {title} = this.props;
     let pickupLocations = [
-      "Front Door",
-      "Back Door",
-      "Shipping",
-      "Receiving",
-      "Office",
-      "Mail Room",
-      "Garage",
-      "Upstairs",
-      "Downstairs",
-      "Guard House",
-      "Third party",
-      "Warehouse"
+      'Front Door',
+      'Back Door',
+      'Shipping',
+      'Receiving',
+      'Office',
+      'Mail Room',
+      'Garage',
+      'Upstairs',
+      'Downstairs',
+      'Guard House',
+      'Third party',
+      'Warehouse',
     ];
     if (this.props.default) {
-      pickupLocations.unshift(this.props.default);
+      pickupLocations.unshift (this.props.default);
     }
 
-    let returnDropdownClass = "dropdown-menu";
+    let returnDropdownClass = 'dropdown-menu';
     if (this.state.returnDropdown) {
-      returnDropdownClass += " show";
+      returnDropdownClass += ' show';
     }
-    let dropdownButtonClass = "btn btn-dropdown-outline dropdown-toggle";
+    let dropdownButtonClass = 'btn btn-dropdown-outline dropdown-toggle';
     if (!this.state.confirmReturn) {
-      dropdownButtonClass += " disabled";
+      dropdownButtonClass += ' disabled';
     }
 
     return (
@@ -98,27 +98,25 @@ class Returns extends Component {
           </label>
         </div>
         <div className="dropdown show">
-          <ClickOutside onClickOutside={e => this.hideReturnDropdown()}>
+          <ClickOutside onClickOutside={e => this.hideReturnDropdown ()}>
             <button
-              onClick={e => this.toggleReturnDropdown()}
+              onClick={e => this.toggleReturnDropdown ()}
               className={dropdownButtonClass}
               type="button"
               data-toggle="dropdown"
               aria-expanded="true"
             >
-              {this.state.selected ? (
-                <React.Fragment>{this.state.selected}</React.Fragment>
-              ) : (
-                <React.Fragment>Preferred Pickup Location</React.Fragment>
-              )}
+              {this.state.selected
+                ? <React.Fragment>{this.state.selected}</React.Fragment>
+                : <React.Fragment>Preferred Pickup Location</React.Fragment>}
             </button>
             <div className={returnDropdownClass}>
-              {pickupLocations.map((item, key) => {
+              {pickupLocations.map ((item, key) => {
                 return (
                   <React.Fragment key={key}>
                     <div
                       className="dropdown-item"
-                      onClick={e => this.handleSelectedReturn(item)}
+                      onClick={e => this.handleSelectedReturn (item)}
                     >
                       <div className="custom-control custom-radio">
                         <input
