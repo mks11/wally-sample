0eccb77e3e3d3ec663873e2a19600d3a3723efad
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2020-01-05 21:38:27 -0500

    Update the How It Works page based on requirements doc.

--- src/pages/HowItWorks.js
+++ src/pages/HowItWorks.js
@@ -14,8 +14,58 @@ class HowItWorks extends Component {
   }
   render() {
     return (
-      <div className="App">
+      <div className="App align-items-center">
          <Title content="Our Process" />
+
+         <section className="page-section aw-our--story align-items-center">
+          <div className="container h-75 w-75">
+            <div className="tagline">
+              <h2>It's what's on the inside that counts.</h2>
+              <p></p>
+              <p>Say goodbye to wasteful packaging with The Wally Shop. Order local, organic produce and we'll deliver it same-day from farmers markets and bulk stores. The best part? We deliver in all reusable packaging, which means no plastic. Ever. Return your packaging during a future delivery, and we'll clean and reuse it.</p>
+              <p>You take care of the earth - we'll take care of the groceries.</p>
+            </div>
+
+            <div className="row d-flex justify-content-center align-items-center">
+              <div className="col-12 col-sm-10 col-md-8 col-lg-6 order-lg-1 order-md-2 order-sm-2 order-2">
+                <div className="w-75 pl-lg-4">
+                  <h1>Order</h1>
+                  <p>Shop produce from local, organic farmers markets & shops</p>
+                </div>
+              </div>
+              <div className="howto-item col-12 col-sm-10 col-md-8 col-lg-6 order-lg-2 order-md-1 order-sm-1 order-1">
+                <img src="images/home6_hd.png" alt=""/>
+              </div>
+            </div>
+
+
+            <div className="row d-flex justify-content-center align-items-center mt-5">
+              <div className="howto-item col-12 col-sm-10 col-md-8 col-lg-6">
+                <img src="images/home7_hd.png" alt=""/>
+              </div>
+              <div className="receive-item receive-div col-12 col-sm-10 col-md-8 col-lg-6 col-lg-offset-2 col-md-offset-2">
+                <div className="receive-item w-75 pull-right">
+                  <h1>Receive</h1>
+                  <p className="receive-item">Get it delivered in all reusable packaging</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="row d-flex mt-5 justify-content-center align-items-center">
+              <div className="col-12 col-sm-10 col-md-8 col-lg-6 order-lg-1 order-md-2 order-sm-2 order-2">
+                <div className="w-75 pl-lg-4">
+                  <h1>Return</h1>
+                  <p>Return packaging at a future delivery for reuse</p>
+                </div>
+              </div>
+              <div className="howto-item col-12 col-sm-10 col-md-8 col-lg-6 order-lg-2 order-md-1 order-sm-1 order-1">
+                <img src="images/home8_hd.png" alt=""/>
+              </div>
+            </div>
+          </div>
+
+          <br /><br />
+        </section>
 
         <section className="page-section aw-our--story">
           <div className="container">
