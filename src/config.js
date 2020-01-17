32f924db94ea9ecd886fb166a87765530023589b
mattyhk <mattkerr@mit.edu>
2020-01-17 13:24:50 -0800

    updated image source. hardcoded unavailable logic for products

--- src/config.js
+++ src/config.js
@@ -147,4 +147,4 @@ export const STRIPE_API_KEY = "pk_test_pq5Ha0elSORzrITfK2G7GkK4";
 export const FB_KEY = "1634254033370820";
 
 export const PRODUCT_BASE_URL =
-  "https://s3.us-east-2.amazonaws.com/the-wally-shop-app/product-images-flat/";
+  "https://s3.us-east-2.amazonaws.com/the-wally-shop-app/product-images-ecomm/";
