df337354f4968e45ff2ccae82d915ffd693530b3
mattyhk <mattkerr@mit.edu>
2020-01-24 09:49:49 -0500

    fixed filename

--- src/aux/getPdf.js
+++ src/aux/getPdf.js
@@ -1,4 +1,4 @@
-const names = ["Tamara Lim"];
+const names = ["Matthew Kerr", "Tamara Lim"];
 const dates = ["Jan 12, 2020", "Feb 1, 2020"];
 const emails = ["theresa.l.morelli@gmail.com"];
 
@@ -9,7 +9,7 @@ const getPdf = async () => {
   for (let i = 0; i < names.length; i++) {
     nameDiv.innerHTML = names[i];
     dateDiv.innerHTML = dates[i];
-
+    let name = names[i];
     // convert to canvas
     const canvas = await html2canvas(document.getElementById("page"));
 
@@ -23,27 +23,28 @@ const getPdf = async () => {
     // embed image in pdf
     await pdf.addImage(canvas.toDataURL("image/png"), "PNG", 0, 0, 11, 8.5);
 
-    // const filename = name.split(" ").join("%20");
-    const data = await pdf.output("arraybuffer"); // after passing to back end, this looks like an empty object
+    let filename = name.split(" ").join("-");
+    // const data = await pdf.output("arraybuffer"); // after passing to back end, this looks like an empty object
     // const data = await pdf.output("blob");     // after passing to back end, this looks like an empty object
     // const data = await pdf.output("datauri");  // too large - causes 'about:blank#blocked'
     // const base64 = window.btoa(data);
-    console.log('data', data)
+    // console.log('data', data)
 
     // Perhaps data needs to be chunked, but I'm not sure how to do that with JS
 
-    try {
-      axios.post("https://localhost:4001/api/admin/certificate", {
-        name: names[i],
-        email: emails[i],
-        data,
-      });
-    } catch (err) {
-      console.error(err)
-    }
+    // try {
+    //   axios.post("https://localhost:4001/api/admin/certificate", {
+    //     name: names[i],
+    //     email: emails[i],
+    //     data,
+    //   });
+    // } catch (err) {
+    //   console.error(err)
+    // }
     
   // save locally
-  // await pdf.save(filename);
+    console.log(filename);
+    await pdf.save(filename);
   }
 };
 
