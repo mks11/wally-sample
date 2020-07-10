16ff0c900f7652a6f7cc3bc6c2b8dc133100737f
Brian Monaccio <brianmonaccio@protonmail.com>
2020-07-10 12:33:44 -0400

    Adds PickPackTab to display all of the day's pick pack orders

---
+++ src/pages/pick-pack-returns/PickPackTab.jsx
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {Card, CardContent, Grid, Typography} from '@material-ui/core';
+import CheckCircle from '@material-ui/icons/CheckCircle';
+import Error from '@material-ui/icons/Error';
+import Cancel from '@material-ui/icons/Cancel';
+import {BASE_URL, API_GET_TODAYS_ORDERS} from '../../config';
+
+// Styles
+import styles from './PickPackTab.module.css';
+
+class PickPackTab extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ordersAndLabels: [],
+    }
+  }
+
+  async componentDidMount(){
+    const url = BASE_URL + API_GET_TODAYS_ORDERS;
+    const res = await axios.get(url);
+    const {ordersAndLabels} = res.data;
+    this.setState({ordersAndLabels})
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className={styles.title}>
+          Pick/Pack Orders
+        </h2>
+        {
+          this.state.ordersAndLabels
+          .sort((a, b) => {
+            // Sort from received -> pending_quality_assurance -> packaged
+            const aStatus = a.status;
+            const bStatus = b.status;
+
+            if (aStatus < bStatus) return 1;
+            else if (aStatus > bStatus) return -1;
+            else return 0;
+          })
+          .map((orderDetails) => {
+            return <OrderCard key={orderDetails.orderId} orderDetails={orderDetails} />
+          })
+        }
+      </div>
+    )
+  }
+}
+
+class OrderCard extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const {orderId, inboundLabel, outboundLabel, status} = this.props.orderDetails;
+
+    return (
+      <Card className={styles.card}>
+        <CardHeader orderId={orderId} status={status} />
+        <Content returnLabel={inboundLabel} orderLabel={outboundLabel} />
+      </Card>
+    )
+  }
+}
+
+class CardHeader extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render(){
+    const {orderId, status} = this.props;
+    return (
+      <Grid container justify={'space-evenly'} alignItems={'center'} className={styles.cardHeader}>
+        <Grid item component={'h3'} className={styles.subTitle}>
+          Order {orderId}
+        </Grid>
+        <Grid item>
+          <StatusIcon status={status}/>
+        </Grid>
+      </Grid>
+    )
+  }
+}
+
+class Content extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    const {orderLabel, returnLabel} = this.props;
+
+    return (
+      <CardContent className={styles.cardContent}>
+        <Grid container justify='space-evenly' alignItems='center'>
+          <Grid item className={styles.labelLink}>
+            <a href={orderLabel} alt="Order Label" >
+              Order Label
+            </a>
+          </Grid>
+          <Grid item className={styles.labelLink}>
+            <a href={returnLabel} alt="Return Label" >
+              Return Label
+            </a>
+          </Grid>
+        </Grid>
+      </CardContent>
+    )
+  }
+}
+
+class StatusIcon extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render(){
+    const {status} = this.props;
+    let color;
+    let text;
+    let Icon;
+
+    switch(status){
+      case 'packaged':
+        Icon = CheckCircle;
+        color = 'green';
+        text = 'Complete';
+        break;
+      case 'pending_quality_assurance':
+        Icon = Error;
+        color = 'yellow';
+        text = 'Pending Quality Assurance';
+        break;
+      default:
+        Icon = Cancel;
+        color = 'red';
+        text = 'Received';
+
+    }
+
+    return (
+    <Grid container direction='column' alignItems='center'>
+      <Icon style={{color}} />
+      <Typography variant='body1' style={{color}}>{text}</Typography>
+    </Grid>
+    )
+  }
+}
+
+export default PickPackTab;
