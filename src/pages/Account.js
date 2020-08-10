ecad3ea1d70fe899619bd761bbcfc8aeccc51846
Brian Monaccio <brianmonaccio@protonmail.com>
2020-08-10 17:58:34 -0400

    Uncomments add address button, and adds firebase config files

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,93 +1,95 @@
-import React, { Component } from 'react';
-import { Input  } from 'reactstrap'
-import Title from '../common/page/Title'
-import AddressModal from './account/AddressModal'
-import PaymentModal from './account/PaymentModal'
-import PromoModal from './account/PromoModal'
-import PromoSuccessModal from './account/PromoSuccessModal'
+import React, { Component } from "react";
+import { Input } from "reactstrap";
+import Title from "../common/page/Title";
+import AddressModal from "./account/AddressModal";
+import PaymentModal from "./account/PaymentModal";
+import PromoModal from "./account/PromoModal";
+import PromoSuccessModal from "./account/PromoSuccessModal";
 
-import { connect } from '../utils'
+import { connect } from "../utils";
 
 class Account extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      name: '',
-      telephone: '',
-      email: '',
+      name: "",
+      telephone: "",
+      email: "",
       editName: true,
       editTelephone: true,
-    }
+    };
 
-
-    this.userStore = this.props.store.user
-    this.uiStore = this.props.store.ui
-    this.modalStore = this.props.store.modal
-
+    this.userStore = this.props.store.user;
+    this.uiStore = this.props.store.ui;
+    this.modalStore = this.props.store.modal;
   }
 
   componentDidMount() {
-    this.userStore.getStatus(true)
-      .then((status) => {
-        if (!status) {
-          this.props.store.routing.push('/')
-        } else {
-          const user = this.userStore.user
-          this.setState({name: user.name,telephone: user.primary_telephone, email: user.email})
-        }
-
-      })
+    this.userStore.getStatus(true).then((status) => {
+      if (!status) {
+        this.props.store.routing.push("/");
+      } else {
+        const user = this.userStore.user;
+        this.setState({
+          name: user.name,
+          telephone: user.primary_telephone,
+          email: user.email,
+        });
+      }
+    });
   }
 
   toggleEditName(s) {
-    this.setState({editName: s})
+    this.setState({ editName: s });
     if (this.state.editName) {
-      const $el = window.$('#inputName')
-      $el.focus()
+      const $el = window.$("#inputName");
+      $el.focus();
     }
   }
 
   toggleEditTelephone(s) {
-    this.setState({editTelephone: s})
+    this.setState({ editTelephone: s });
     if (this.state.editTelephone) {
-      const $el = window.$('#inputTelephone')
-      $el.focus()
+      const $el = window.$("#inputTelephone");
+      $el.focus();
     }
   }
 
   edit() {
-    this.userStore.edit({
-      name: this.state.name,
-      telephone: this.state.telephone
-    }).then((data) => {
-      this.userStore.setUserData(data)
-    }).catch((e) => {
-      const msg = e.response.data.error.message
-      this.setState({invalidText: msg})
-      console.error('Failed to delete address', e)
-    })
+    this.userStore
+      .edit({
+        name: this.state.name,
+        telephone: this.state.telephone,
+      })
+      .then((data) => {
+        this.userStore.setUserData(data);
+      })
+      .catch((e) => {
+        const msg = e.response.data.error.message;
+        this.setState({ invalidText: msg });
+        console.error("Failed to delete address", e);
+      });
   }
 
   updateName(e) {
-    this.edit()
-    this.toggleEditName(true)
+    this.edit();
+    this.toggleEditName(true);
   }
 
   updateTelephone(e) {
-    this.edit()
-    this.toggleEditTelephone(true)
+    this.edit();
+    this.toggleEditTelephone(true);
   }
 
-
   render() {
-    if (!this.userStore.user) return null
+    if (!this.userStore.user) return null;
 
-    const name = this.state.name
-    const telephone = this.state.telephone ? this.state.telephone : ''
+    const name = this.state.name;
+    const telephone = this.state.telephone ? this.state.telephone : "";
 
-    const addresses = this.userStore.user.addresses
-    const payments = this.userStore.user.payment
-      
+    const addresses = this.userStore.user.addresses;
+    const payments = this.userStore.user.payment;
+
     return (
       <div className="App">
         <Title content="Account" />
@@ -104,21 +106,55 @@ class Account extends Component {
                   placeholder="Enter your name"
                   id="inputName"
                   value={name}
-                  onChange={(e) => this.setState({name: e.target.value})}/>
-                { this.state.editName ? <button type="button" className="btn btn-transparent" onClick={e => this.toggleEditName(false)}>EDIT</button> : null }
-                { !this.state.editName ? <button type="button" className="btn btn-transparent" onClick={e => this.updateName(e)}>SAVE</button> : null }
+                  onChange={(e) => this.setState({ name: e.target.value })}
+                />
+                {this.state.editName ? (
+                  <button
+                    type="button"
+                    className="btn btn-transparent"
+                    onClick={(e) => this.toggleEditName(false)}
+                  >
+                    EDIT
+                  </button>
+                ) : null}
+                {!this.state.editName ? (
+                  <button
+                    type="button"
+                    className="btn btn-transparent"
+                    onClick={(e) => this.updateName(e)}
+                  >
+                    SAVE
+                  </button>
+                ) : null}
               </div>
               <div className="aw-input--group">
-              <Input
-                className="aw-input--control aw-input--control-large aw-input--left"
-                type="text"
-                readOnly={this.state.editTelephone}
-                id="inputTelephone"
-                placeholder="Enter your phone number"
-                value={telephone}
-                onChange={(e) => this.setState({telephone: e.target.value})}/>
-                { this.state.editTelephone ? <button type="button" className="btn btn-transparent" onClick={e => this.toggleEditTelephone(false)}>EDIT</button> : null }
-                { !this.state.editTelephone ? <button type="button" className="btn btn-transparent" onClick={e => this.updateTelephone(e)}>SAVE</button> : null }
+                <Input
+                  className="aw-input--control aw-input--control-large aw-input--left"
+                  type="text"
+                  readOnly={this.state.editTelephone}
+                  id="inputTelephone"
+                  placeholder="Enter your phone number"
+                  value={telephone}
+                  onChange={(e) => this.setState({ telephone: e.target.value })}
+                />
+                {this.state.editTelephone ? (
+                  <button
+                    type="button"
+                    className="btn btn-transparent"
+                    onClick={(e) => this.toggleEditTelephone(false)}
+                  >
+                    EDIT
+                  </button>
+                ) : null}
+                {!this.state.editTelephone ? (
+                  <button
+                    type="button"
+                    className="btn btn-transparent"
+                    onClick={(e) => this.updateTelephone(e)}
+                  >
+                    SAVE
+                  </button>
+                ) : null}
               </div>
               <Input
                 className="aw-input--control aw-input--control-large aw-input--left"
@@ -126,7 +162,7 @@ class Account extends Component {
                 readOnly={true}
                 value={this.state.email}
                 placeholder="Enter your email address"
-                />
+              />
             </form>
           </div>
         </section>
@@ -134,22 +170,37 @@ class Account extends Component {
         <section className="page-section aw-account--address pt-2">
           <div className="container">
             <h2>Addresses</h2>
-              <ul className="list-addresses">
+            <ul className="list-addresses">
               {addresses.map((data, index) => (
-                  <li key={index}>
-                    <span className="addresses--address">{data.street_address} {data.unit}, {data.state} {data.zip}</span>
-                    <span className="addresses--info">{data.name}</span>
-                    <span className="addresses--info">{data.telephone}</span>
-                      <span className="addresses--default button">
-                        {data.address_id === this.userStore.user.preferred_address ? (
-                          <span onClick={e => this.userStore.showAddressModal(data)}>DEFAULT</span>
-                        ): null}
-                        <i className="ico ico-arrow-right ml-3 button" onClick={e => this.userStore.showAddressModal(data)}></i>
+                <li key={index}>
+                  <span className="addresses--address">
+                    {data.street_address} {data.unit}, {data.state} {data.zip}
+                  </span>
+                  <span className="addresses--info">{data.name}</span>
+                  <span className="addresses--info">{data.telephone}</span>
+                  <span className="addresses--default button">
+                    {data.address_id ===
+                    this.userStore.user.preferred_address ? (
+                      <span
+                        onClick={(e) => this.userStore.showAddressModal(data)}
+                      >
+                        DEFAULT
                       </span>
+                    ) : null}
+                    <i
+                      className="ico ico-arrow-right ml-3 button"
+                      onClick={(e) => this.userStore.showAddressModal(data)}
+                    ></i>
+                  </span>
                 </li>
               ))}
-              </ul>
-              {/* <button className="btn btn-icon-transparent mt-4" onClick={e => this.userStore.showAddressModal()}><i className="ico ico-add-circle mr-3"></i>Add new address</button> */}
+            </ul>
+            <button
+              className="btn btn-icon-transparent mt-4"
+              onClick={(e) => this.userStore.showAddressModal()}
+            >
+              <i className="ico ico-add-circle mr-3"></i>Add new address
+            </button>
           </div>
         </section>
 
@@ -160,23 +211,40 @@ class Account extends Component {
               {payments.map((data, index) => (
                 <li key={index}>
                   <span className="payments--card">*****{data.last4}</span>
-                      <span className="addresses--default button">
-                        {data._id === this.userStore.user.preferred_payment ? (
-                          <span onClick={e => this.userStore.showPaymentModal(data)}>DEFAULT</span>
-                        ): null}
-                        <i className="ico ico-arrow-right ml-3 button" onClick={e => this.userStore.showPaymentModal(data)}></i>
+                  <span className="addresses--default button">
+                    {data._id === this.userStore.user.preferred_payment ? (
+                      <span
+                        onClick={(e) => this.userStore.showPaymentModal(data)}
+                      >
+                        DEFAULT
                       </span>
+                    ) : null}
+                    <i
+                      className="ico ico-arrow-right ml-3 button"
+                      onClick={(e) => this.userStore.showPaymentModal(data)}
+                    ></i>
+                  </span>
                 </li>
               ))}
             </ul>
-            <button onClick={e=>this.userStore.showPaymentModal()} className="btn btn-icon-transparent btn-block mt-4"><i className="ico ico-add-square mr-3"></i>Add new card</button>
-            <button onClick={e=>this.userStore.togglePromoModal()} className="btn btn-icon-transparent btn-block mt-4"><i className="ico ico-add-square mr-3"></i>Add promo or gift card</button>
+            <button
+              onClick={(e) => this.userStore.showPaymentModal()}
+              className="btn btn-icon-transparent btn-block mt-4"
+            >
+              <i className="ico ico-add-square mr-3"></i>Add new card
+            </button>
+            <button
+              onClick={(e) => this.userStore.togglePromoModal()}
+              className="btn btn-icon-transparent btn-block mt-4"
+            >
+              <i className="ico ico-add-square mr-3"></i>Add promo or gift card
+            </button>
           </div>
         </section>
-        { this.userStore.addressModalOpen ? <AddressModal/> : null }
-        { this.userStore.paymentModalOpen ? <PaymentModal/> : null }
-        <PromoModal/>
-        <PromoSuccessModal/>
+        {this.userStore.addressModalOpen ? <AddressModal /> : null}
+        {this.userStore.paymentModalOpen ? <PaymentModal /> : null}
+        <PromoModal />
+        <PromoSuccessModal />
       </div>
     );
   }
