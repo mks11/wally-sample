b1a473eafa5292f058253960f48d3899f75b217a
Mukul K Singh <mukul.intel@gmail.com>
2020-08-28 15:58:12 +0530

    no similar products page if cart empty

---
+++ src/pages/SimilarProducts/EmptyCartMessage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Grid } from "@material-ui/core";
+
+export default function EmptyCartMessage() {
+  return (
+    <Grid container>
+      <Grid item xs={11}>
+        <Typography variant="h1" align={"left"}>
+          Oops, it looks like your cart is empty!
+        </Typography>
+      </Grid>
+      <Grid item xs={11}>
+        <Typography variant="body1" align={"left"}>
+          Why don’t you{" "}
+          <Link to="/main" className={"color-purple"}>
+            head back to our shop
+          </Link>{" "}
+          so we can fix that?
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+}
