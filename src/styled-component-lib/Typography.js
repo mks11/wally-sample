f009ab96478eaba2b9e3f8db10721732d387d9d5
Brian Monaccio <brianmonaccio@protonmail.com>
2020-08-29 11:26:27 -0400

    Renames styled components typography file

--- src/styled-component-lib/Typography.js
+++ src/styled-component-lib/Typography.js
@@ -4,3 +4,7 @@ import { Typography } from '@material-ui/core';
 export const SuccessText = styled(Typography)`
   color: #3acf3f;
 `;
+
+export const ErrorText = styled(Typography)`
+  color: #f50056;
+`;
