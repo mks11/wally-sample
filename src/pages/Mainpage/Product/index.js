cb8de08a1742995978bfd81cdc087727d264d2c3
Brian Monaccio <brianmonaccio@protonmail.com>
2020-09-03 18:23:10 -0400

    Removes hardcoded width from product images so they will fill the width of their column

--- src/pages/Mainpage/Product/index.js
+++ src/pages/Mainpage/Product/index.js
@@ -1,47 +1,47 @@
-import React from 'react'
-import { PRODUCT_BASE_URL } from 'config'
-import { formatMoney } from 'utils'
-import { LazyLoadImage } from 'react-lazy-load-image-component'
+import React from 'react';
+import { PRODUCT_BASE_URL } from 'config';
+import { formatMoney } from 'utils';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
-const Product = props => {
-  const { product, onProductClick, deliveryTimes } = props
-  const producer = product.producer || null
-  const price = product.product_price / 100
+const Product = (props) => {
+  const { product, onProductClick, deliveryTimes } = props;
+  const producer = product.producer || null;
+  const price = product.product_price / 100;
 
-  let price_unit = 'per jar'
+  let price_unit = 'per jar';
 
-  const outOfStock = product.out_of_stock
+  const outOfStock = product.out_of_stock;
   return (
-    <div className="col-lg-3 col-md-4 col-6 col-sm-6 product-thumbnail" onClick={() => onProductClick(product.product_id, deliveryTimes)}>
+    <div
+      className="col-lg-3 col-md-4 col-6 col-sm-6 product-thumbnail"
+      onClick={() => onProductClick(product.product_id, deliveryTimes)}
+    >
       <LazyLoadImage
         alt={product.product_name || product.name}
-        height={180}
         src={`${PRODUCT_BASE_URL}${product.image_refs[0]}`}
-        width={200}
       />
       <div className="row product-detail">
-        <div className="col-3 product-price">
-          {formatMoney(price)}
-        </div>
-        <div className="col-9 product-weight">
-          {price_unit}
-        </div>
+        <div className="col-3 product-price">{formatMoney(price)}</div>
+        <div className="col-9 product-weight">{price_unit}</div>
       </div>
-      { producer && <span className="product-producer">{producer}</span>}
+      {producer && <span className="product-producer">{producer}</span>}
       <br></br>
-      { product.product_name && <span className="product-desc"><strong>{product.product_name}</strong></span>}
-      { product.name && <span className="product-desc"><strong>{product.name}</strong></span>}
-      {
-      }
-      <div className={`product-packaged ${(outOfStock) ? 'out-of-stock' : ''}`}>
-        {
-          outOfStock
-            ? 'Out of stock'
-            : `packed in ${product.packaging_type}`
-        }
+      {product.product_name && (
+        <span className="product-desc">
+          <strong>{product.product_name}</strong>
+        </span>
+      )}
+      {product.name && (
+        <span className="product-desc">
+          <strong>{product.name}</strong>
+        </span>
+      )}
+      {}
+      <div className={`product-packaged ${outOfStock ? 'out-of-stock' : ''}`}>
+        {outOfStock ? 'Out of stock' : `packed in ${product.packaging_type}`}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
