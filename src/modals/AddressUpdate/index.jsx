fcf5e291b010ffc68cd7402d716d0c38f6cc110d
Brian Monaccio <brianmonaccio@protonmail.com>
2020-09-04 18:07:38 -0400

    Makes stylistic changes to address update modal.

--- src/modals/AddressUpdate/index.jsx
+++ src/modals/AddressUpdate/index.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import AddressUpdateForm from './AddressUpdateForm';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Container, Typography } from '@material-ui/core';
 
 function UpdateModal({ stores, toggle }) {
   return (
-    <>
-      <div className="modal-header">
-        <Typography variant="h2"> Edit Address </Typography>
-      </div>
+    <Container maxWidth="md">
+      <Typography variant="h1"> Edit Address </Typography>
       <Box padding={2}>
         <AddressUpdateForm store={stores} toggle={toggle} />
       </Box>
-    </>
+    </Container>
   );
 }
 
