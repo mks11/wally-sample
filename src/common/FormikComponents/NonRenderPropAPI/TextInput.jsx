c1ddddd7d029c759bd96cda731007b88eb47ad6a
Brian Monaccio <brianmonaccio@protonmail.com>
2020-09-04 12:37:20 -0400

    Adds empty line to HelperText in the TextInput component so it's less janky

--- src/common/FormikComponents/NonRenderPropAPI/TextInput.jsx
+++ src/common/FormikComponents/NonRenderPropAPI/TextInput.jsx
@@ -5,11 +5,12 @@ import { HelperText } from 'styled-component-lib/HelperText';
 
 const FormikTextInput = (props) => {
   const [field, meta] = useField(props);
+
   return (
     <>
       <TextField error={!!(meta.touched && meta.error)} {...field} {...props} />
       <HelperText error={!!(meta.touched && meta.error)}>
-        {meta.touched && meta.error}
+        {meta.touched && meta.error ? meta.error : ' '}
       </HelperText>
     </>
   );
