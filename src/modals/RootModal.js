bc6f97e6adac725c24b42c44dca41a1dbbbf9259
Mukul K Singh <mukul.intel@gmail.com>
2020-09-04 22:09:57 +0530

    updated root modal for Address update and delete

--- src/modals/RootModal.js
+++ src/modals/RootModal.js
@@ -26,6 +26,8 @@ import JoinWaitlistModal from './JoinWaitlistModal';
 import RedeemDepositModal from './RedeemDepositModal';
 import SchedulePickupModal from './SchedulePickupModal';
 import PackagingDepositModal from './PackagingDepositModal';
+import AddressUpdateModal from './AddressUpdate';
+import AddressDeleteModal from './AddressDelete';
 import SuccessModal from './SuccessModal';
 
 const ModalRoutes = {
@@ -54,6 +56,8 @@ const ModalRoutes = {
   redeemdeposit: RedeemDepositModal,
   packagingdeposit: PackagingDepositModal,
   success: SuccessModal,
+  addressUpdate: AddressUpdateModal,
+  addressDelete: AddressDeleteModal,
 };
 
 class RootModal extends Component {
