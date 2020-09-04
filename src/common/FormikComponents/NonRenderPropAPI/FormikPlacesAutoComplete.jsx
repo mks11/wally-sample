cfc6554b110f5e0d8c45922e64ac78f207b77aac
Brian Monaccio <brianmonaccio@protonmail.com>
2020-09-04 18:13:24 -0400

    Added key to autocomplete suggestions, changed component name bc it was overloading PlacesAutocomplete component, fixed proptypes, and removed interpolation function used to set value of PlacesAutocomplete component

--- src/common/FormikComponents/NonRenderPropAPI/FormikPlacesAutoComplete.jsx
+++ src/common/FormikComponents/NonRenderPropAPI/FormikPlacesAutoComplete.jsx
@@ -1,30 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import PlacesAutocomplete, {
   geocodeByAddress,
 } from 'react-places-autocomplete';
 import { useFormikContext } from 'formik';
 
-export default function PlacesAutoComplete({ names, mode = 'edit' }) {
+// Components
+import { Grid } from '@material-ui/core';
+import { Info } from '@material-ui/icons';
+import { Label } from 'styled-component-lib/InputLabel';
+
+export default function FormikPlacesAutoComplete({ names, mode = 'edit' }) {
   const [address, setAddress] = useState('');
-  const { values: formikValues, setFieldValue } = useFormikContext();
+  const { setFieldValue } = useFormikContext();
 
   const handleChange = (address) => {
     setAddress(address);
   };
-
-  const encodeAddressFromValues = () => {
-    const { streetAddress, unit, city, state, country } = formikValues;
-    if (unit) {
-      return `${streetAddress}, ${unit}, ${city}, ${state} ${country}`;
-    } else {
-      return `${streetAddress}, ${city}, ${state} ${country}`;
-    }
-  };
-
-  useEffect(() => {
-    setAddress(encodeAddressFromValues());
-  }, []);
 
   const setFormikValuesByName = (address) => {
     names.forEach((name) => {
@@ -33,7 +25,6 @@ export default function PlacesAutoComplete({ names, mode = 'edit' }) {
   };
 
   const handleSelect = (address) => {
-    setAddress(address);
     geocodeByAddress(address)
       .then((results) => {
         const address = getAddressComponents(results[0]);
@@ -43,48 +34,61 @@ export default function PlacesAutoComplete({ names, mode = 'edit' }) {
   };
 
   return (
-    <PlacesAutocomplete
-      value={address}
-      onChange={handleChange}
-      onSelect={handleSelect}
-      debounce={600}
-    >
-      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-        <div style={{ position: 'relative' }}>
-          <input
-            {...getInputProps({
-              // readOnly: mode === 'edit',
-              autoComplete: 'off',
-              placeholder: 'Delivery to...',
-              className:
-                'aw-input--control aw-input--control-large aw-input--left location-search-input  aw-input--location aw-input--bordered mt-3 form-control',
-            })}
-          />
-          <div className="autocomplete-dropdown-container">
-            {suggestions.map((suggestion) => {
-              const className = suggestion.active
-                ? 'suggestion-item--active'
-                : 'suggestion-item';
-              // inline style for demonstration purpose
-              const style = suggestion.active
-                ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                : { backgroundColor: '#ffffff', cursor: 'pointer' };
-              return (
-                <div
-                  {...getSuggestionItemProps(suggestion, {
-                    className,
-                    style,
-                  })}
-                >
-                  <strong>{suggestion.formattedSuggestion.mainText}</strong>{' '}
-                  <small>{suggestion.formattedSuggestion.secondaryText}</small>
-                </div>
-              );
-            })}
+    <>
+      <Grid container alignItems="center">
+        <Grid item style={{ marginRight: '0.5rem' }}>
+          <Label>Search for an address</Label>
+        </Grid>
+        <Grid item>
+          <Info />
+        </Grid>
+      </Grid>
+      <PlacesAutocomplete
+        value={address}
+        onChange={handleChange}
+        onSelect={handleSelect}
+        debounce={600}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps }) => (
+          <div style={{ position: 'relative' }}>
+            <input
+              {...getInputProps({
+                // readOnly: mode === 'edit',
+                autoComplete: 'off',
+                placeholder: 'Delivery to...',
+                className:
+                  'aw-input--control aw-input--control-large aw-input--left location-search-input  aw-input--location aw-input--bordered mt-3 form-control',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {suggestions.map((suggestion, idx) => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                    key={`suggestion-${idx}`}
+                  >
+                    <strong>{suggestion.formattedSuggestion.mainText}</strong>{' '}
+                    <small>
+                      {suggestion.formattedSuggestion.secondaryText}
+                    </small>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-        </div>
-      )}
-    </PlacesAutocomplete>
+        )}
+      </PlacesAutocomplete>
+    </>
   );
 }
 
@@ -94,29 +98,31 @@ function getAddressComponents(place) {
     route: 'long_name',
     locality: 'long_name',
     administrative_area_level_1: 'short_name',
-    country: 'long_name',
+    country: 'short_name',
     postal_code: 'short_name',
   };
 
+  // Select the correct address components to build out the address with.
+  const { address_components } = place;
   const address = {};
-
-  for (let i = 0; i < place.address_components.length; i++) {
-    const addressType = place.address_components[i].types[0];
-    if (componentForm[addressType]) {
-      const val = place.address_components[i][componentForm[addressType]];
+  for (let component of address_components) {
+    const addressType = component.types[0];
+    const addressComponent = componentForm[addressType];
+    if (addressComponent) {
+      const val = component[addressComponent];
       address[addressType] = val;
     }
   }
 
-  const city = address.locality || address.administrative_area_level_1;
-  const state = address.administrative_area_level_1;
-  const country = address.country;
-  const zip = address.postal_code || '';
-  const streetAddress = `${address.street_number} ${address.route}`; //TODO more testing
-
-  return { city, state, country, zip, streetAddress };
+  return {
+    streetAddress: `${address.street_number} ${address.route}`,
+    city: address.locality || address.administrative_area_level_1,
+    state: address.administrative_area_level_1,
+    zip: address.postal_code || '',
+    country: address.country,
+  };
 }
 
-PlacesAutoComplete.propTypes = {
-  names: PropTypes.oneOf[('city', 'state', 'country', 'zip', 'streetAddress')],
+FormikPlacesAutoComplete.propTypes = {
+  names: PropTypes.array.isRequired,
 };
