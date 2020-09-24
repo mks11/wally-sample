58c1581c31fb88743e456cc43a2dfd6dc44ec251
Brian Monaccio <brianmonaccio@protonmail.com>
2020-09-24 06:25:37 -0400

    Reduces complexity of ImageCarousel component

--- src/modals/ProductModal/ImageCarousel.jsx
+++ src/modals/ProductModal/ImageCarousel.jsx
@@ -1,8 +1,7 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Box } from '@material-ui/core';
 import {
-  CarouselContext,
   CarouselProvider,
   Slider,
   Slide,
@@ -16,32 +15,6 @@ import { PRODUCT_BASE_URL } from 'config';
 
 // Icons
 import { ChevronLeft, ChevronRight } from '@material-ui/icons';
-export default function ImageCarousel({
-  imageRefs,
-  ingredientLabels,
-  name,
-  nutritionFacts,
-  productId,
-  ...props
-}) {
-  var slides = imageRefs;
-  if (nutritionFacts) slides = [...slides, ...nutritionFacts];
-  if (ingredientLabels) slides = [...slides, ...ingredientLabels];
-
-  // Only allow 5 slides
-  slides = slides.slice(0, 6);
-
-  return (
-    <CarouselProvider
-      naturalSlideWidth={362}
-      naturalSlideHeight={362}
-      totalSlides={slides.length}
-      style={{ backgroundColor: 'transparent' }}
-    >
-      <Slides productId={productId} slides={slides} />
-    </CarouselProvider>
-  );
-}
 
 const BackSlideControl = styled(ButtonBack)`
   position: absolute;
@@ -83,50 +56,58 @@ const Thumbnail = styled(Dot)`
   }
 `;
 
-function Slides({ productId, slides }) {
-  const carouselContext = useContext(CarouselContext);
-  const [currentSlide, setCurrentSlide] = useState(
-    carouselContext.state.currentSlide,
-  );
+export default function ImageCarousel({
+  imageRefs,
+  ingredientLabels,
+  name,
+  nutritionFacts,
+  productId,
+  ...props
+}) {
+  var slides = imageRefs;
+  if (nutritionFacts) slides = [...slides, ...nutritionFacts];
+  if (ingredientLabels) slides = [...slides, ...ingredientLabels];
 
-  useEffect(() => {
-    function onChange() {
-      setCurrentSlide(carouselContext.state.currentSlide);
-    }
-    carouselContext.subscribe(onChange);
-    return () => carouselContext.unsubscribe(onChange);
-  }, [carouselContext]);
+  // Only allow 5 slides
+  slides = slides.slice(0, 6);
 
   return (
-    <Box position={'relative'}>
-      <Slider>
-        {slides.map((imgRef, idx) => {
-          const src = PRODUCT_BASE_URL + productId + '/' + imgRef;
-          return (
-            <Slide key={imgRef} index={idx}>
-              <Image src={src} alt={name} />
-            </Slide>
-          );
-        })}
-      </Slider>
-      <BackSlideControl>
-        <ChevronLeft fontSize="large" />
-      </BackSlideControl>
-      <NextSlideControl>
-        <ChevronRight fontSize="large" />
-      </NextSlideControl>
-      <Box p={1} display="flex">
-        {slides.map((imgRef, idx) => {
-          const src = PRODUCT_BASE_URL + productId + '/' + imgRef;
-          return (
-            <Box p={1} flex="20%" flexGrow={0} flexShrink={0}>
-              <Thumbnail slide={idx}>
+    <CarouselProvider
+      naturalSlideWidth={362}
+      naturalSlideHeight={362}
+      totalSlides={slides.length}
+      style={{ backgroundColor: 'transparent' }}
+    >
+      <Box position={'relative'}>
+        <Slider>
+          {slides.map((imgRef, idx) => {
+            const src = PRODUCT_BASE_URL + productId + '/' + imgRef;
+            return (
+              <Slide key={imgRef} index={idx}>
                 <Image src={src} alt={name} />
-              </Thumbnail>
-            </Box>
-          );
-        })}
+              </Slide>
+            );
+          })}
+        </Slider>
+        <BackSlideControl>
+          <ChevronLeft fontSize="large" />
+        </BackSlideControl>
+        <NextSlideControl>
+          <ChevronRight fontSize="large" />
+        </NextSlideControl>
+        <Box p={1} display="flex">
+          {slides.map((imgRef, idx) => {
+            const src = PRODUCT_BASE_URL + productId + '/' + imgRef;
+            return (
+              <Box p={1} flex="20%" flexGrow={0} flexShrink={0}>
+                <Thumbnail slide={idx}>
+                  <Image src={src} alt={name} />
+                </Thumbnail>
+              </Box>
+            );
+          })}
+        </Box>
       </Box>
-    </Box>
+    </CarouselProvider>
   );
 }
