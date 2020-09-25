1baf4401dc8e741a8e2ba8edb03b5857f4110691
Brian Monaccio <brianmonaccio@protonmail.com>
2020-09-25 17:46:17 -0400

    Implements new RootModal that has improved default styling and accepts and renders children

---
+++ src/modals/RootModalV2.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+// Node Modlues
+import { isMobile } from 'react-device-detect';
+
+// Material ui
+import { Box, Button, Drawer, Grid } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+
+// mobx
+import { useStores } from 'hooks/mobx';
+import { observer } from 'mobx-react';
+
+function RootModalV2() {
+  const { store } = useStores();
+  const { isOpen, children } = store.modalV2;
+
+  const handleClose = () => {
+    store.modalV2.close();
+  };
+
+  return (
+    <Drawer
+      anchor="top"
+      open={isOpen}
+      onClose={handleClose}
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      PaperProps={{
+        square: false,
+        style: {
+          minWidth: isMobile ? '100%' : '450px',
+          top: 'unset',
+          right: 'unset',
+          left: 'unset',
+          bottom: 'unset',
+        },
+      }}
+    >
+      <Box px={4} py={2}>
+        <Grid container justify="flex-end">
+          <Button onClick={handleClose}>
+            <CloseIcon fontSize="large" />
+          </Button>
+        </Grid>
+        {children}
+      </Box>
+    </Drawer>
+  );
+}
+
+export default observer(RootModalV2);
