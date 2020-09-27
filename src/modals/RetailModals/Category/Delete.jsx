cb13397086346932bc494a7f06ef2d1c242058d3
Mukul K Singh <mukul.intel@gmail.com>
2020-09-27 16:45:29 +0530

    implements delete Category modal

--- src/modals/RetailModals/Category/Delete.jsx
+++ src/modals/RetailModals/Category/Delete.jsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Confirmation from 'modals/shared/Confirmation';
+import useDelete from 'common/hooks/useDelete';
+import { API_CATEGORIES_DELETE } from 'config';
 
-function Delete(props) {
-  return <div></div>;
+function Delete({ stores: { modal, ...store }, ...props }) {
+  const id = modal.modalData;
+  const [deleteCat, { success }] = useDelete(
+    `${API_CATEGORIES_DELETE}/${id}`,
+    store,
+  );
+
+  useEffect(() => {
+    if (success) {
+      props.toggle();
+    }
+  }, [success]);
+
+  const handleConfirm = () => {
+    deleteCat();
+  };
+  const handleCancel = () => {
+    props.toggle(); // close the modal
+  };
+
+  return (
+    <Confirmation
+      title="Delete Category"
+      message="Are you sure you want to delete this category?"
+      onCancel={handleCancel}
+      onConfirm={handleConfirm}
+    />
+  );
 }
 
 Delete.propTypes = {};
