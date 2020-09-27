c51c20eb0d394419d7bd707721fb2c8dd5eaa2de
Mukul K Singh <mukul.intel@gmail.com>
2020-09-27 16:38:23 +0530

    fixed subcategory and closing modal as sideeffect

--- src/modals/RetailModals/Subcategory/Add.jsx
+++ src/modals/RetailModals/Subcategory/Add.jsx
@@ -17,25 +17,29 @@ import useGet from 'common/hooks/useGet';
 import getIdNamePair from './../getIdNamePair';
 
 function Add({ stores: store, ...props }) {
-  const cats = useRequest(store, async () => store.retail.getCategories());
+  const cats =
+    useRequest(store, async () => store.retail.getCategories()) || [];
   const { data: packaging } = useGet(API_PACKAGING_LIST, store);
 
-  const [
-    addSubcategory,
-    { loading: formLoading, error: submitError },
-  ] = usePost(API_SUBCATEGORIES_POST, store);
+  const [addSubcategory, { success, loading: posting }] = usePost(
+    API_SUBCATEGORIES_POST,
+    store,
+  );
+
+  useEffect(() => {
+    if (success) {
+      props.toggle();
+    }
+  }, [success]);
 
   const handleSubmit = async (values, { setSubmitting }) => {
     addSubcategory(values);
-    if (!formLoading) {
+    if (!posting) {
       setSubmitting(false);
-    }
-    if (!formLoading && !submitError) {
-      props.toggle();
     }
   };
 
-  const cat_ids = cats.map((v) => v._id);
+  const cat_ids = cats.map((v) => v.category_id);
 
   return (
     <FormWrapper title="Add Subcategory">
@@ -61,7 +65,7 @@ function Add({ stores: store, ...props }) {
                 name="parent_ids"
                 label="Categories"
                 values={cat_ids}
-                valueToDisplayMap={getIdNamePair(cats)}
+                valueToDisplayMap={getIdNamePair(cats, 'category_id', 'name')}
               />
             </Grid>
             <Grid item xs={12}>
