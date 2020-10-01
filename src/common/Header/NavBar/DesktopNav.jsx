157f9cec76eac013a02646e3095a39b13abc2240
Brian Monaccio <brianmonaccio@protonmail.com>
2020-10-01 13:21:39 -0400

    Adds the new ops nav to the desktop nav component

--- src/common/Header/NavBar/DesktopNav.jsx
+++ src/common/Header/NavBar/DesktopNav.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 // Custom Components
 import { DesktopUserNav } from './UserNav';
 import { DesktopAdminNav } from './AdminNav';
+import { DesktopOpsNav } from './OpsNav';
 
 function DesktopNav() {
   return (
     <>
       <DesktopUserNav />
       <DesktopAdminNav />
+      <DesktopOpsNav />
     </>
   );
 }
