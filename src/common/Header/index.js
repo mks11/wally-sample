7aa680f74ee63b37e40f3d5b96c8527c38708b08
Brian Monaccio <brianmonaccio@protonmail.com>
2020-10-02 17:59:30 -0400

    Adds observer wrapper to logo component so it will send the user to the correct home page based on their log in status

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 // Hooks
 import { useStores } from 'hooks/mobx';
+import { observer } from 'mobx-react';
 
 // React Router
 import { Link } from 'react-router-dom';
@@ -16,6 +17,40 @@ import styled from 'styled-components';
 
 // Custom Components
 import Navbar from 'common/Header/NavBar';
+
+const LogoFull = styled.img`
+  height: 32px;
+
+  @media only screen and (width >= 567px) {
+    height: 40px;
+  }
+
+  @media only screen and (width >= 768px) {
+    height: 44px;
+  }
+
+  @media only screen and (width >= 992px) {
+    height: 48px;
+  }
+`;
+
+const Logo = observer(() => {
+  const { user, product } = useStores();
+
+  var home = user.status ? '/main' : '/';
+
+  const onLogoClick = () => {
+    product.resetSearch();
+  };
+
+  return (
+    <Box>
+      <Link to={home} onClick={onLogoClick}>
+        <LogoFull src="/images/logo-full.svg" alt="The Wally Shop W logo." />
+      </Link>
+    </Box>
+  );
+});
 
 export default function Header() {
   return (
@@ -37,40 +72,6 @@ export default function Header() {
           </Grid>
         </Grid>
       </Container>
-    </Box>
-  );
-}
-
-const LogoFull = styled.img`
-  height: 32px;
-
-  @media only screen and (width >= 567px) {
-    height: 40px;
-  }
-
-  @media only screen and (width >= 768px) {
-    height: 44px;
-  }
-
-  @media only screen and (width >= 992px) {
-    height: 48px;
-  }
-`;
-
-function Logo() {
-  const { user, product } = useStores();
-
-  var home = user.user ? '/main' : '/';
-
-  const onLogoClick = () => {
-    product.resetSearch();
-  };
-
-  return (
-    <Box>
-      <Link to={home} onClick={onLogoClick}>
-        <LogoFull src="/images/logo-full.svg" alt="The Wally Shop W logo." />
-      </Link>
     </Box>
   );
 }
