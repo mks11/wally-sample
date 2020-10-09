8f03e50785aad67bdaaae0dc8efa293a2128711e
Brian Monaccio <brianmonaccio@protonmail.com>
2020-10-09 09:49:56 -0400

    Removes unused address management code from the product top bar

--- src/pages/Mainpage/ProductTop/index.js
+++ src/pages/Mainpage/ProductTop/index.js
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Row, Col, Container } from "reactstrap";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Container } from 'reactstrap';
 
-import { logEvent } from "services/google-analytics";
-import { connect } from "utils";
+import { connect } from 'utils';
 
-import Filters from "./Filters";
-import SearchBar from "./SearchBar";
-import CartDropdown from "./CartDropdown";
+import Filters from './Filters';
+import SearchBar from './SearchBar';
+import CartDropdown from './CartDropdown';
 
 class ProductTop extends Component {
   constructor(props) {
@@ -21,12 +20,6 @@ class ProductTop extends Component {
     this.modalStore = props.store.modal;
 
     this.state = {
-      selectedAddressChanged: false,
-      selectedTimeChanged: false,
-      selectedAddress: this.userStore.selectedDeliveryAddress,
-      selectedTime: this.userStore.selectedDeliveryTime,
-      fakeUser: this.userStore.loadFakeUser(),
-
       sticky: 0,
     };
   }
@@ -37,18 +30,18 @@ class ProductTop extends Component {
     $(document).ready(function () {
       self.calculateSticyPosition();
     });
-    $(window).bind("scroll", this.handleFixedTop);
-    $(window).bind("resize", this.calculateSticyPosition);
+    $(window).bind('scroll', this.handleFixedTop);
+    $(window).bind('resize', this.calculateSticyPosition);
   }
 
   componentWillUnmount() {
     const $ = window.$;
-    $(window).unbind("scroll", this.handleFixedTop);
-    $(window).unbind("resize", this.calculateSticyPosition);
+    $(window).unbind('scroll', this.handleFixedTop);
+    $(window).unbind('resize', this.calculateSticyPosition);
   }
 
   calculateSticyPosition = () => {
-    const element = document.getElementsByClassName("aw-header")[0];
+    const element = document.getElementsByClassName('aw-header')[0];
     const newSticky = element ? element.offsetHeight : 0;
     this.setState({
       sticky: newSticky,
@@ -60,21 +53,17 @@ class ProductTop extends Component {
     const { sticky } = this.state;
     const stickyPos = sticky;
     if (window.pageYOffset >= stickyPos) {
-      $(".product-top").addClass("fixed");
+      $('.product-top').addClass('fixed');
     } else {
-      $(".product-top").removeClass("fixed");
+      $('.product-top').removeClass('fixed');
     }
   };
 
-  formatAddress(address) {
-    return address ? address.substr(0, 25).concat("...") : null;
-  }
-
   handleCheckout = () => {
     if (this.userStore.status) {
-      this.routing.push("/main/similar-products");
+      this.routing.push('/main/similar-products');
     } else {
-      this.modalStore.toggleModal("login");
+      this.modalStore.toggleModal('login');
     }
   };
 
@@ -83,135 +72,24 @@ class ProductTop extends Component {
       .showModal(
         data.product_id,
         data.customer_quantity,
-        this.userStore.getDeliveryParams()
+        this.userStore.getDeliveryParams(),
       )
       .then((data) => {
         this.userStore.adjustDeliveryTimes(
           data.delivery_date,
-          this.checkoutStore.deliveryTimes
+          this.checkoutStore.deliveryTimes,
         );
-        this.modalStore.toggleModal("product");
+        this.modalStore.toggleModal('product');
       });
   };
 
   handleDelete = (id) => {
-    this.modalStore.toggleModal("delete", id);
+    this.modalStore.toggleModal('delete', id);
   };
 
   handleMobileSearchOpen = () => {
     const { onMobileSearchClick } = this.props;
     onMobileSearchClick && onMobileSearchClick();
-  };
-
-  handleAddNewAddress = async (data) => {
-    const {
-      newContactName,
-      newState,
-      newDeliveryNotes,
-      newZip,
-      newAptNo,
-      newCity,
-      newCountry,
-      newPhoneNumber,
-      newStreetAddress,
-      newPreferedAddress,
-    } = data;
-
-    const dataMap = {
-      name: newContactName,
-      state: newState,
-      delivery_notes: newDeliveryNotes,
-      zip: newZip,
-      unit: newAptNo,
-      city: newCity,
-      country: newCountry,
-      telephone: newPhoneNumber,
-      street_address: newStreetAddress,
-      preferred_address: newPreferedAddress,
-    };
-
-    if (!this.userStore.user) {
-      if (!this.zipStore.validateZipCode(newZip)) {
-        let err = {
-          response: { data: { error: { message: "Invalid zip code" } } },
-        };
-        throw err;
-      }
-
-      this.userStore.addFakeAddress(dataMap);
-      const fakeUser = this.userStore.loadFakeUser();
-      this.setState({ fakeUser });
-
-      return fakeUser;
-    }
-
-    const response = await this.userStore.saveAddress(dataMap);
-    const address = this.userStore.selectedDeliveryAddress;
-    this.handleSubmitAddress(address);
-    return response;
-  };
-
-  handleSubmitAddress = async (address) => {
-    this.modalStore.showDeliveryChange("address", {
-      address,
-    });
-    this.userStore.setDeliveryAddress(address);
-    return;
-  };
-
-  handleSelectAddress = (data) => {
-    const selectedAddress = this.userStore.selectedDeliveryAddress;
-    if (!selectedAddress || selectedAddress.address_id !== data.address_id) {
-      this.setState({ selectedAddress: data, selectedAddressChanged: true });
-    } else {
-      this.setState({ selectedAddressChanged: false });
-    }
-  };
-
-  handleSubmitDeliveryAddress = () => {
-    logEvent({ category: "DeliveryOptions", action: "ClickEditAddressChoice" });
-    if (!this.state.selectedAddressChanged) {
-      return;
-    }
-    this.checkoutStore.getDeliveryTimes().then(() => {
-      this.modalStore.showDeliveryChange("address", {
-        address: this.state.selectedAddress,
-        times: this.checkoutStore.deliveryTimes,
-      });
-    });
-  };
-
-  handleSelectTime = (data) => {
-    const selectedTime = this.userStore.selectedDeliveryTime;
-    if (
-      !selectedTime ||
-      selectedTime.date !== data.date ||
-      selectedTime.time !== data.time ||
-      selectedTime.day !== data.day
-    ) {
-      this.setState({ selectedTime: data, selectedTimeChanged: true });
-      logEvent({ category: "DeliveryOptions", action: "ClickEditTimeChoice" });
-    } else {
-      this.setState({ selectedTimeChanged: false });
-    }
-  };
-
-  handleSubmitDeliveryTime = () => {
-    logEvent({ category: "DeliveryOptions", action: "ClickEditTimeChoice" });
-    if (!this.state.selectedTimeChanged) {
-      return;
-    }
-
-    this.setState({ selectedTimeChanged: false });
-    this.modalStore.showDeliveryChange("time", this.state.selectedTime);
-  };
-
-  handleMouseEnter = () => {
-    this.uiStore.showBackdrop();
-  };
-
-  handleMouseLeave = () => {
-    this.uiStore.hideBackdrop();
   };
 
   handleFiltersSelect = (values) => {
@@ -260,4 +138,4 @@ class ProductTop extends Component {
   }
 }
 
-export default connect("store")(ProductTop);
+export default connect('store')(ProductTop);
