e5d2bd3b5849e30fbf4a0973b6e21343d3490adb
Brian Monaccio <brianmonaccio@protonmail.com>
2020-10-16 12:32:09 -0400

    Disables quantity select in product modal if product sold out.

--- src/common/QuantitySelect.js
+++ src/common/QuantitySelect.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import { FormGroup, Input } from 'reactstrap'
+import React from 'react';
+import { FormGroup, Input } from 'reactstrap';
 
-const QuantitySelect = ({
-  value,
-  onSelectChange,
-  options,
-  price_unit,
-}) => {
+const QuantitySelect = ({ value, onSelectChange, options, price_unit }) => {
   return (
     <FormGroup style={{ maxWidth: '140px' }}>
-      <Input type="select" value={value} onChange={onSelectChange}>
-        { options.map((v, i) => (<option key={i} value={v}>{`${v} ${price_unit}`}</option>)) }
+      <Input
+        type="select"
+        value={value}
+        onChange={onSelectChange}
+        disabled={options && !options.length ? true : false}
+      >
+        {options.map((v, i) => (
+          <option key={i} value={v}>{`${v} ${price_unit}`}</option>
+        ))}
       </Input>
     </FormGroup>
-  )
-}
+  );
+};
 
-export default QuantitySelect
+export default QuantitySelect;
