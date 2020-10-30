c452cb3e7aa260b4a7ac3fced6a50ec53acf8087
Mukul K Singh <mukul.intel@gmail.com>
2020-10-30 17:00:48 +0530

    (in-progress) removed some unused states and code

--- src/pages/Checkout/DeliveryAddressOptions/index.jsx
+++ src/pages/Checkout/DeliveryAddressOptions/index.jsx
@@ -5,20 +5,16 @@ import PlacesAutocomplete, {
 } from 'react-places-autocomplete';
 import AddressCreateForm from 'forms/Address/Create';
 import { PrimaryWallyButton } from 'styled-component-lib/Buttons';
+import { Box, Button, Card, Typography } from '@material-ui/core';
+import AddressRadioItem from './AddressRadioItem';
+import { Add } from '@material-ui/icons';
 
 class AddressOptions extends Component {
   state = {
     error: false,
-    newStreetAddress: '',
-    newAptNo: '',
-    newZip: '',
     newContactName: '',
     newPhoneNumber: '',
-    newDeliveryNotes: '',
-    newState: '',
-    newCity: '',
-    newCountry: '',
-    newPreferedAddress: false,
+    selected: '',
   };
 
   componentDidMount() {
@@ -32,29 +28,29 @@ class AddressOptions extends Component {
     this.setState({ selected });
   }
 
-  handleSelectAddress(address_id) {
+  handleSelectAddress = (address_id) => {
     this.setState({ selected: address_id });
     if (address_id === '0') {
+      // TODO handle the case here ...why are the following states here
       this.setState({
         newAddress: true,
         newContactName: this.props.user.name,
         newPhoneNumber: this.props.user.primary_telephone,
       });
     } else {
-      this.setState({ newAddress: false });
       const address = this.props.user.addresses.find(
         (d) => d._id === address_id,
       );
       this.props.onSelect && this.props.onSelect(address);
     }
-  }
+  };
 
   handleSubmitAddress = () => {
     if (!this.state.selected) return;
     if (this.props.locking) {
-      this.setState({ invalidSelectAddress: null, lock: true });
+      this.setState({ lock: true });
     } else {
-      this.setState({ invalidSelectAddress: null, lock: false });
+      this.setState({ lock: false });
     }
     const address = this.props.user.addresses.find(
       (d) => d._id === this.state.selected,
@@ -62,125 +58,17 @@ class AddressOptions extends Component {
     this.props.onSubmit &&
       this.props.onSubmit(address).catch((e) => {
         if (e.response && e.response.data.error) {
-          this.setState({
-            invalidSelectAddress: e.response.data.error.message,
-          });
+          // this.setState({
+          //   invalidSelectAddress: e.response.data.error.message,
+          // });
+          //TODO appropriate replacement of the commented out?
         }
         console.error(e);
       });
   };
 
-  handleAddNewAddress = () => {
-    this.setState({ invalidText: null });
-    if (!this.state.newStreetAddress) {
-      this.setState({ invalidText: 'Street address cannot be empty' });
-      return;
-    }
-
-    if (!this.state.newContactName) {
-      this.setState({ invalidText: 'Name cannot be empty' });
-      return;
-    }
-
-    if (!this.state.newPhoneNumber) {
-      this.setState({ invalidText: 'Telephone cannot be empty' });
-      return;
-    }
-
-    this.props
-      .onAddNew(this.state)
-      .then((data) => {
-        const lastAddress = data.addresses[data.addresses.length - 1];
-        this.setState({
-          selected: lastAddress.address_id,
-          newAddress: false,
-          invalidText: '',
-          newStreetAddress: '',
-          newAptNo: '',
-          newZip: '',
-          newContactName: '',
-          newPhoneNumber: '',
-          newDeliveryNotes: '',
-          newState: '',
-          newCity: '',
-          newCountry: '',
-          newPreferedAddress: false,
-        });
-
-        this.props.onSelect && this.props.onSelect(lastAddress);
-      })
-      .catch((e) => {
-        if (e.response && e.response.data.error) {
-          const msg = e.response.data.error.message;
-          this.setState({ invalidText: msg });
-        }
-        console.error('Failed to save address', e);
-      });
-  };
-
   handleAddAddress = () => {
-    //todo
-  };
-
-  fillInAddress(place) {
-    var componentForm = {
-      street_number: 'short_name',
-      route: 'long_name',
-      locality: 'long_name',
-      sublocality_level_1: 'long_name',
-      administrative_area_level_1: 'short_name',
-      country: 'long_name',
-      postal_code: 'short_name',
-    };
-
-    let address = {};
-
-    for (var i = 0; i < place.address_components.length; i++) {
-      var addressType = place.address_components[i].types[0];
-
-      // Brooklyn and other parts of New York City do not include the city as part of the address.
-      // Instead, they use sublocality_level_1.
-      var addressTypes = place.address_components[i].types;
-      if (addressTypes.includes('sublocality_level_1')) {
-        addressType = 'sublocality_level_1';
-      }
-
-      if (componentForm[addressType]) {
-        var val = place.address_components[i][componentForm[addressType]];
-        address[addressType] = val;
-      }
-    }
-
-    let city = address.locality;
-    if (!city && address.sublocality_level_1) {
-      city = address.sublocality_level_1;
-    }
-    const state = address.administrative_area_level_1;
-    const country = address.country;
-    const zip = address.postal_code;
-
-    const streetAddress = [address.street_number, address.route].join(' ');
-
-    this.setState({
-      newStreetAddress: streetAddress,
-      newCity: city,
-      newState: state,
-      newCountry: country,
-      newZip: zip,
-    });
-  }
-
-  handleNewAddressChange = (newStreetAddress) => {
-    this.setState({ newStreetAddress });
-  };
-
-  handleNewAddressSelect = (newStreetAddress) => {
-    this.setState({ newStreetAddress });
-    geocodeByAddress(newStreetAddress)
-      .then((results) => {
-        this.fillInAddress(results[0]);
-      })
-      .catch((error) => console.error('Error', error));
+    //todo: when the component is changed to functional
   };
 
   unlock = () => {
@@ -189,16 +77,6 @@ class AddressOptions extends Component {
   };
 
   render() {
-    let addressFormClass = 'addAdressForm mb-4';
-    if (!this.state.newAddress) {
-      addressFormClass += ' d-none';
-    }
-
-    let addressCardClass = 'card1 delivery';
-    if (this.state.error) {
-      addressCardClass += ' error';
-    }
-
     let selected = this.state.selected
       ? this.state.selected
       : this.props.selected;
@@ -218,81 +96,55 @@ class AddressOptions extends Component {
       typeof this.props.button !== 'undefined' ? this.props.button : true;
 
     return (
-      <React.Fragment>
+      <>
         {showTitle && (
-          <h3 className="m-0 mb-3 p-r">
-            {this.props.title ? this.props.title : 'Delivery address'}
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="h4" gutterBottom>
+              {this.props.title ? this.props.title : 'Delivery address'}
+            </Typography>
             {lock && editable ? (
-              <a
-                onClick={this.unlock}
-                className="address-rbtn link-blue pointer"
-              >
-                CHANGE
-              </a>
+              <Button onClick={this.unlock}>
+                <Typography variant="h6">CHANGE</Typography>
+              </Button>
             ) : null}
-          </h3>
+          </Box>
         )}
-        <div className={addressCardClass}>
-          <div className={'card-body' + (lock ? ' lock' : '')}>
-            <PrimaryWallyButton onClick={this.handleAddAddress}>
-              Add New Address
-            </PrimaryWallyButton>
-
-            {data.map((data, index) => {
-              if (lock && selected !== data.address_id) {
-                return null;
-              }
-              return (
-                <div
-                  className={
-                    'custom-control custom-radio bb1' +
-                    (data.address_id === selected ? ' active' : '')
-                  }
-                  key={index}
-                >
-                  <input
-                    type="radio"
-                    id={'address-' + index}
-                    name="customRadio"
-                    checked={data.address_id === selected}
-                    className="custom-control-input"
-                    value={data.address_id}
-                    onChange={(e) => this.handleSelectAddress(data.address_id)}
+        <Box mb={4}>
+          <Card>
+            <Box p={2}>
+              <Box mb={2}>
+                <PrimaryWallyButton onClick={this.handleAddAddress}>
+                  <Add /> Add New Address
+                </PrimaryWallyButton>
+              </Box>
+              {data.map((data, index) => {
+                if (lock && selected !== data.address_id) {
+                  return null;
+                }
+                return (
+                  <AddressRadioItem
+                    data={data}
+                    key={data.address_id}
+                    index={index}
+                    selected={selected}
+                    onChange={this.handleSelectAddress}
+                    isPreferredAddress={preferred_address === data.address_id}
                   />
-                  <label
-                    className="custom-control-label"
-                    htmlFor={'address-' + index}
-                    onClick={(e) => this.handleSelectAddress(data.address_id)}
-                  >
-                    {data.street_address} {data.unit}, {data.state} {data.zip}
-                    <div className="address-phone">
-                      {data.name}, {data.telephone}
-                    </div>
-                  </label>
-                  {preferred_address === data.address_id && (
-                    <a className="address-rbtn link-blue">DEFAULT</a>
-                  )}
-                </div>
-              );
-            })}
+                );
+              })}
 
-            <AddressCreateForm />
-
-            {showButton && (
-              <React.Fragment>
-                {!lock && !this.state.newAddress ? (
-                  <button
-                    className="btn btn-main active"
-                    onClick={(e) => this.handleSubmitAddress(e)}
-                  >
-                    SUBMIT
-                  </button>
-                ) : null}
-              </React.Fragment>
-            )}
-          </div>
-        </div>
-      </React.Fragment>
+              {showButton && !lock ? (
+                <PrimaryWallyButton
+                  className="btn btn-main active"
+                  onClick={this.handleSubmitAddress}
+                >
+                  SUBMIT
+                </PrimaryWallyButton>
+              ) : null}
+            </Box>
+          </Card>
+        </Box>
+      </>
     );
   }
 }
