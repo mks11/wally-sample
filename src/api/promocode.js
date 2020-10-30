5a86b92beda76a32e7924c0d59b48a29b3b0d699
Brian Monaccio <brianmonaccio@protonmail.com>
2020-10-30 17:43:22 -0400

    Replaces referral API route with promo code API route

--- src/api/promocode.js
+++ src/api/promocode.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { BASE_URL } from 'config';
 
 // REFERRAL ENDPOINTS
-const APPLY_PROMO = BASE_URL + '/api/referral';
+const APPLY_PROMO = BASE_URL + '/api/promocode';
 
 export const applyPromo = (promoCode, auth) => {
-  return axios.post(APPLY_PROMO, { promoCode }, auth);
+  return axios.put(APPLY_PROMO, { promoCode }, auth);
 };
