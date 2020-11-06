f331c51fe933df97d85f651b7be7c1daf749279b
Mukul K Singh <mukul.intel@gmail.com>
2020-11-06 19:07:04 +0530

    WIP added CheckoutForm

---
+++ src/pages/Checkout/CheckoutForm.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { useStores } from 'hooks';
+import DeliveryAddressOptions from './FormikDeliveryAddressOptions';
+
+export default function CheckoutForm() {
+  return (
+    <Formik
+      initialValues={{
+        addressId,
+        shippingServiceLevel: 'ups_ground',
+        paymentId,
+      }}
+      validationSchema={{
+        addressId: Yup.string(),
+        shippingServiceLevel: Yup.string().isRequired(),
+        paymentId: Yup.string(),
+      }}
+      enableReinitialize
+    >
+      <DeliveryAddressOptions name="addressId" />
+      <PrimaryWallyButton type="submit">Checkout</PrimaryWallyButton>
+    </Formik>
+  );
+}
