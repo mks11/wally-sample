0d3012b9aefa5ce09341bec97acd704f3d0cd707
Brian Monaccio <brianmonaccio@protonmail.com>
2020-11-06 19:52:36 -0500

    Removes unused hook from CheckoutForm

--- src/pages/Checkout/CheckoutForm.jsx
+++ src/pages/Checkout/CheckoutForm.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
-import { useStores } from 'hooks';
 import DeliveryAddressOptions from './FormikDeliveryAddressOptions';
 
 export default function CheckoutForm() {
