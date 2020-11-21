f248cf67c599191099f137edaf15c814b582b32e
Brian Monaccio <brianmonaccio@protonmail.com>
2020-11-21 13:52:22 -0500

    Adds keyboard arrow icon to the Icons file.

--- src/Icons/index.js
+++ src/Icons/index.js
@@ -1,5 +1,6 @@
 import AddIcon from '@material-ui/icons/Add';
 import CloseIcon from '@material-ui/icons/Close';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import RemoveIcon from '@material-ui/icons/Remove';
 
-export { AddIcon, CloseIcon, RemoveIcon };
+export { AddIcon, CloseIcon, KeyboardArrowDownIcon, RemoveIcon };
