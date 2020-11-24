208bfd0f52a2fc23b8eee5492ada1318fba811cc
Brian Monaccio <brianmonaccio@protonmail.com>
2020-11-23 19:32:30 -0500

    Small adjustments to HowItWorks.

--- src/pages/HowItWorks.js
+++ src/pages/HowItWorks.js
@@ -16,6 +16,7 @@ import ResponsivePhoto from 'common/ResponsivePhoto';
 
 import { Box, Typography } from '@material-ui/core';
 import Page from './shared/Page';
+import PageSection from 'common/PageSection';
 
 function HowItWorks() {
   return (
@@ -24,62 +25,65 @@ function HowItWorks() {
       description="Learn about The Wally Shop service."
       content="Our Process"
     >
-      <Box textAlign="center" maxWidth="720px" marginX="auto">
-        <Typography variant="h2" gutterBottom>
-          It's what's on the inside that counts.
-        </Typography>
-        <Typography gutterBottom>
-          We are introducing a whole new way to shop sustainably. Our vision is
-          to help you shop for everything (Bulk foods! Beauty products!
-          Household products!) conveniently in all reusable packaging. We’re
-          starting with responsibly-made, Trader Joe’s price-competitive bulk
-          foods, but we will be expanding categories and on-boarding more brands
-          in the coming weeks. We want to get you what you need, 100% waste
-          free, so please reach out if you have any brands in mind ;)
-        </Typography>
-        <Typography gutterBottom>
-          We hope you’re as ready as we are to join the #reusablesrevolution and
-          change the world in dreamy purple ~ one order at a time.
-          #wallydreamsinpurple
-        </Typography>
-      </Box>
-
-      <HowTo
-        title="Order"
-        description="Choose from hundreds of responsibly-made, Trader Joe’s
+      <PageSection>
+        <Box px={2}>
+          <Typography variant="h2" gutterBottom>
+            It's what's on the inside that counts.
+          </Typography>
+          <Typography>
+            We're introducing a whole new way to shop sustainably. Our vision is
+            to help you shop for everything (Bulk foods! Beauty products!
+            Household products!) conveniently in all reusable packaging. We’re
+            starting with responsibly-made, Trader Joe’s price-competitive bulk
+            foods, but we will be expanding categories and on-boarding more
+            brands constantly. We want to get you what you need, 100% waste
+            free, so please{' '}
+            <a href="mailto: info@thewallyshop.co" alt="reach out to us.">
+              reach out
+            </a>{' '}
+            if you have any brands in mind.
+          </Typography>
+        </Box>
+      </PageSection>
+      <PageSection>
+        <HowTo
+          title="Order"
+          description="Choose from hundreds of responsibly-made, Trader Joe’s
                   price-competitive bulk foods. At checkout, you will be charged
                   a deposit for your packaging (don’t worry, you will be getting
                   it back!)."
-        photo={
-          <ResponsivePhoto
-            mobileSrc={orderHd450}
-            desktopSrc={orderHd600}
-            alt="Man giving money in exchange for a jar of pasta."
-          />
-        }
-      />
-
-      <HowTo
-        title="Receive"
-        description="
+          photo={
+            <ResponsivePhoto
+              mobileSrc={orderHd450}
+              desktopSrc={orderHd600}
+              alt="Man giving money in exchange for a jar of pasta."
+            />
+          }
+        />
+      </PageSection>
+      <PageSection>
+        <HowTo
+          title="Receive"
+          description="
                   Your order will arrive at your doorstep in completely
                   reusable, returnable packaging. The shipping tote it arrives
                   in folds up for easy storage. Simple, convenient, 100% waste
                   free shopping.
             "
-        photoAlign="left"
-        photo={
-          <ResponsivePhoto
-            mobileSrc={toteHd450}
-            desktopSrc={toteHd600}
-            alt="The Wally Shop's reusable tote."
-          />
-        }
-      />
-
-      <HowTo
-        title="Return"
-        description="
+          photoAlign="left"
+          photo={
+            <ResponsivePhoto
+              mobileSrc={toteHd450}
+              desktopSrc={toteHd600}
+              alt="The Wally Shop's reusable tote."
+            />
+          }
+        />
+      </PageSection>
+      <PageSection>
+        <HowTo
+          title="Return"
+          description="
                 Once finished, you can return all your packaging (jars, totes,
                 anything we send to you, we take back and reuse) to a
                 FedEx/UPS delivery courier on a future delivery or schedule a
@@ -87,14 +91,15 @@ function HowItWorks() {
                 you and the packaging is cleaned to be put back into
                 circulation.
             "
-        photo={
-          <ResponsivePhoto
-            mobileSrc={returnPackagingHd450}
-            desktopSrc={returnPackagingHd600}
-            alt="Returning an empty jar."
-          />
-        }
-      />
+          photo={
+            <ResponsivePhoto
+              mobileSrc={returnPackagingHd450}
+              desktopSrc={returnPackagingHd600}
+              alt="Returning an empty jar."
+            />
+          }
+        />
+      </PageSection>
     </Page>
   );
 }
