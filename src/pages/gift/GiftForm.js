b3665c2bd9b39b2a39fb27bc417381f58c19ec41
Mukul K Singh <mukul.intel@gmail.com>
2020-11-30 17:46:10 +0530

    converted class to functional

--- src/pages/gift/GiftForm.js
+++ src/pages/gift/GiftForm.js
@@ -1,105 +1,45 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Button, FormGroup, Label, Input } from 'reactstrap';
 import CurrencyInput from 'react-currency-input-field';
 import AmountGroup from 'common/AmountGroup';
-import { validateEmail } from 'utils';
 import PaymentSelect from 'common/PaymentSelect';
 
-class GiftForm extends Component {
-  constructor(props) {
-    super(props);
+function GiftForm(props) {
+  const [customGiftAmount, setCustomGiftAmount] = useState(false);
+  const [lockPayment, setLockPayment] = useState(false);
 
-    this.state = {
-      customGiftAmount: false,
-      lockPayment: false,
+  const [giftAmount, setGiftAmount] = useState('');
+  const [giftPayment, setGiftPayment] = useState(null);
+  const [giftTo, setGiftTo] = useState('');
+  const [giftFrom, setGiftFrom] = useState('');
+  const [giftMessage, setGiftMessage] = useState('');
 
-      giftAmount: '',
-      giftPayment: null,
-      giftTo: '',
-      giftFrom: '',
-      giftMessage: '',
+  const [successMessage, setSuccessMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [formIsValid, setFormIsValid] = useState(false);
 
-      successMessage: null,
-      errorMessage: null,
-      formIsValid: false,
-    };
-  }
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    if (!prevState.giftFrom && nextProps.giftFrom !== prevState.giftFrom) {
-      return {
-        giftFrom: nextProps.giftFrom,
-      };
-    } else return prevState;
-  }
-
-  validateForm = ({ beforeSend = false }) => {
-    const { giftTo, giftFrom, giftAmount, lockPayment } = this.state;
-
-    if (beforeSend) {
-      if (!giftAmount.length) {
-        this.setState({
-          errorMessage: 'Please select the amount',
-          formIsValid: false,
-        });
-        return;
-      }
-
-      if (!lockPayment) {
-        this.setState({
-          errorMessage: 'Please select payment',
-          formIsValid: false,
-        });
-        return;
-      }
-
-      if (!validateEmail(giftTo) || !validateEmail(giftFrom)) {
-        const errorMessage = `Invalid ${
-          !validateEmail(giftTo) ? 'recipient' : 'sender'
-        } email`;
-        this.setState({
-          errorMessage,
-          formIsValid: false,
-        });
-        return;
-      }
-
-      this.setState({ formIsValid: true });
-    } else {
-      if (giftAmount.length && giftTo.length && giftFrom.length) {
-        this.setState({ formIsValid: true });
-      }
+  useEffect(() => {
+    if (!giftFrom && props.giftFrom !== giftFrom) {
+      setGiftFrom(props.giftFrom);
     }
+  }, [props.giftFrom, giftFrom]);
+
+  const handlePaymentSubmit = (lock, selectedPayment) => {
+    setLockPayment(lock);
+    setGiftPayment(selectedPayment);
   };
 
-  handlePaymentSubmit = (lock, selectedPayment) => {
-    this.setState({
-      lockPayment: lock,
-      giftPayment: selectedPayment,
-    });
-  };
-
-  handleAddPayment = (data) => {
-    const { onAddPayment, userGuest } = this.props;
+  const handleAddPayment = (data) => {
+    const { onAddPayment, userGuest } = props;
     onAddPayment && onAddPayment(data);
 
     if (data) {
-      this.setState({ lockPayment: userGuest });
+      setLockPayment(userGuest);
     }
   };
 
-  handleGiftCheckoutSubmit = (e) => {
-    const { onSubmit } = this.props;
-    const {
-      giftAmount,
-      giftPayment,
-      giftTo,
-      giftFrom,
-      giftMessage,
-      formIsValid,
-    } = this.state;
-
-    this.validateForm({ beforeSend: true });
+  const handleGiftCheckoutSubmit = (e) => {
+    const { onSubmit } = props;
 
     const giftAmountStr = giftAmount.replace('$', '');
     const giftNumberAmount = parseFloat(giftAmountStr).toFixed(2) * 100;
@@ -117,6 +57,9 @@ class GiftForm extends Component {
     e.preventDefault();
   };
 
+  /* TODO 
+  leaving the commented as this is to replace with Formik
+  
   handleInputChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value.toString(),
@@ -124,161 +67,135 @@ class GiftForm extends Component {
       errorMessage: null,
     });
   };
+  */
 
-  handleAmountChange = (value) => {
-    this.setState({
-      customGiftAmount: false,
-      giftAmount: value.toString(),
-      formIsValid: false,
-      errorMessage: null,
-    });
+  const handleAmountChange = (value) => {
+    setCustomGiftAmount(false);
+    setGiftAmount(value.toString());
+    setFormIsValid(false);
+    setErrorMessage(null);
   };
 
-  handleCustomAmounClick = () => {
-    this.setState({ customGiftAmount: true });
+  const handleCustomAmounClick = () => {
+    setCustomGiftAmount(true);
   };
 
-  render() {
-    const {
-      customGiftAmount,
-      errorMessage,
-      successMessage,
-      formIsValid,
-      lockPayment,
-      giftFrom,
-      giftAmount,
-    } = this.state;
-    const {
-      userPayment,
-      userPreferredPayment,
-      userGuest,
-      customErrorMsg,
-      forceSelect,
-    } = this.props;
+  const {
+    userPayment,
+    userPreferredPayment,
+    userGuest,
+    customErrorMsg,
+    forceSelect,
+  } = props;
 
-    return (
-      <div className="gift-card-form">
-        <FormGroup row>
-          <Label for="giftAmount" sm={3} className="text-md-right">
-            Amount
-          </Label>
-          <Col sm={9}>
-            <AmountGroup
-              amountClick={this.handleAmountChange}
-              customClick={this.handleCustomAmounClick}
-              values={[25, 50, 100]}
-              selected={50}
+  return (
+    <div className="gift-card-form">
+      <FormGroup row>
+        <Label for="giftAmount" sm={3} className="text-md-right">
+          Amount
+        </Label>
+        <Col sm={9}>
+          <AmountGroup
+            amountClick={handleAmountChange}
+            customClick={handleCustomAmounClick}
+            values={[25, 50, 100]}
+            selected={50}
+            prefix="$"
+            custom={true}
+            product={false}
+          />
+          {customGiftAmount ? (
+            <CurrencyInput
+              allowDecimals
+              className="form-control"
+              id="giftAmount"
+              name="giftAmount"
               prefix="$"
-              custom={true}
-              product={false}
+              value={giftAmount}
             />
-            {customGiftAmount ? (
-              <CurrencyInput
-                allowDecimals
-                className="form-control"
-                id="giftAmount"
-                name="giftAmount"
-                onBlur={this.validateForm}
-                onChange={(value, name) =>
-                  this.setState({
-                    [name]: value,
-                    formIsValid: false,
-                    errorMessage: null,
-                  })
-                }
-                prefix="$"
-                value={giftAmount}
-              />
-            ) : null}
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label sm={3} className="text-md-right">
-            Credit Card
-          </Label>
-          <Col sm={9}>
-            <PaymentSelect
-              {...{
-                lockPayment,
-                userPayment,
-                userPreferredPayment,
-                onAddPayment: this.handleAddPayment,
-                onSubmitPayment: this.handlePaymentSubmit,
-                userGuest,
-                forceSelect,
-              }}
-            />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="giftTo" sm={3} className="text-md-right">
-            To
-          </Label>
-          <Col sm={9}>
-            <Input
-              type="email"
-              name="giftTo"
-              id="giftTo"
-              placeholder="Enter recipient's email address"
-              onChange={this.handleInputChange}
-              onBlur={this.validateForm}
-            />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="giftFrom" sm={3} className="text-md-right">
-            From
-          </Label>
-          <Col sm={9}>
-            <Input
-              type="email"
-              name="giftFrom"
-              id="giftFrom"
-              value={giftFrom}
-              placeholder="Enter your email address"
-              onChange={this.handleInputChange}
-              onBlur={this.validateForm}
-            />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Label for="giftMessage" sm={3} className="text-md-right">
-            Your message
-          </Label>
-          <Col sm={9}>
-            <Input
-              type="textarea"
-              name="giftMessage"
-              id="giftMessage"
-              placeholder="Write a message for your recipient (optional)"
-              onChange={this.handleInputChange}
-              onBlur={this.validateForm}
-            />
-          </Col>
-        </FormGroup>
-        <FormGroup row>
-          <Col sm={{ size: 9, offset: 3 }}>
-            <Button
-              className={`gift-submit ${formIsValid ? 'active' : ''}`}
-              onClick={this.handleGiftCheckoutSubmit}
-            >
-              Purchase Gift Card
-            </Button>
-            {(errorMessage || customErrorMsg) && (
-              <div className="text-error text-center mt-2">
-                {errorMessage || customErrorMsg}
-              </div>
-            )}
-            {successMessage && (
-              <div className="text-success text-center mt-2">
-                {successMessage}
-              </div>
-            )}
-          </Col>
-        </FormGroup>
-      </div>
-    );
-  }
+          ) : null}
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label sm={3} className="text-md-right">
+          Credit Card
+        </Label>
+        <Col sm={9}>
+          <PaymentSelect
+            {...{
+              lockPayment,
+              userPayment,
+              userPreferredPayment,
+              onAddPayment: handleAddPayment,
+              onSubmitPayment: handlePaymentSubmit,
+              userGuest,
+              forceSelect,
+            }}
+          />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="giftTo" sm={3} className="text-md-right">
+          To
+        </Label>
+        <Col sm={9}>
+          <Input
+            type="email"
+            name="giftTo"
+            id="giftTo"
+            placeholder="Enter recipient's email address"
+          />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="giftFrom" sm={3} className="text-md-right">
+          From
+        </Label>
+        <Col sm={9}>
+          <Input
+            type="email"
+            name="giftFrom"
+            id="giftFrom"
+            value={giftFrom}
+            placeholder="Enter your email address"
+          />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Label for="giftMessage" sm={3} className="text-md-right">
+          Your message
+        </Label>
+        <Col sm={9}>
+          <Input
+            type="textarea"
+            name="giftMessage"
+            id="giftMessage"
+            placeholder="Write a message for your recipient (optional)"
+          />
+        </Col>
+      </FormGroup>
+      <FormGroup row>
+        <Col sm={{ size: 9, offset: 3 }}>
+          <Button
+            className={`gift-submit ${formIsValid ? 'active' : ''}`}
+            onClick={handleGiftCheckoutSubmit}
+          >
+            Purchase Gift Card
+          </Button>
+          {(errorMessage || customErrorMsg) && (
+            <div className="text-error text-center mt-2">
+              {errorMessage || customErrorMsg}
+            </div>
+          )}
+          {successMessage && (
+            <div className="text-success text-center mt-2">
+              {successMessage}
+            </div>
+          )}
+        </Col>
+      </FormGroup>
+    </div>
+  );
 }
 
 export default GiftForm;
