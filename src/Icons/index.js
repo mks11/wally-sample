8b94657fb3d52c95b1779a0963a0c464b84ba31f
Brian Monaccio <brianmonaccio@protonmail.com>
2020-12-01 13:16:00 -0500

    Lowers font size of search icon in search bar so the bar isn't broken.

--- src/Icons/index.js
+++ src/Icons/index.js
@@ -3,5 +3,13 @@ import CloseIcon from '@material-ui/icons/Close';
 import InfoIcon from '@material-ui/icons/Info';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import RemoveIcon from '@material-ui/icons/Remove';
+import SearchIcon from '@material-ui/icons/Search';
 
-export { AddIcon, CloseIcon, InfoIcon, KeyboardArrowDownIcon, RemoveIcon };
+export {
+  AddIcon,
+  CloseIcon,
+  InfoIcon,
+  KeyboardArrowDownIcon,
+  RemoveIcon,
+  SearchIcon,
+};
