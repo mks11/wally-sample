309404f96e2268782cc4b5bbd7d7a3c3588e6f75
mks11 <mukul.intel@gmail.com>
2021-01-14 23:37:46 +0530

    with requested changes

--- src/common/ProductAssortment/SortAndFilterMenu/__tests__/sorting-functions.spec.js
+++ src/common/ProductAssortment/SortAndFilterMenu/__tests__/sorting-functions.spec.js
@@ -2,6 +2,8 @@ import {
   sortAlphabetically,
   sortByNewness,
   sortByPrice,
+  ASCENDING,
+  DESCENDING,
 } from './../sorting-functions';
 
 import {
@@ -18,6 +20,16 @@ const randomlyOrdered = [
   naturalLiquidHandSoapPeppermint, // Price 1428, 2020-10-21T17:40:12.237Z
   naturalLaundryDetergentLavender, // Price 762,  2020-10-21T17:40:11.499Z
   seaMineralsBlueIrisHandSoap, // Price 472,      2020-11-30T16:51:05.284Z
+];
+
+const randomlyOrderedWithDuplicates = [
+  citrusLavenderSoapBar, // Price 700,   2020-11-05T15:15:48.098Z
+  laundryConcentratePaste, // Price 2150 ,  2020-11-30T16:55:05.143Z
+  naturalLiquidHandSoapPeppermint, // Price 1428, 2020-10-21T17:40:12.237Z
+  naturalLaundryDetergentLavender, // Price 762,  2020-10-21T17:40:11.499Z
+  seaMineralsBlueIrisHandSoap, // Price 472,      2020-11-30T16:51:05.284Z
+  citrusLavenderSoapBar, // Price 700,   2020-11-05T15:15:48.098Z
+  naturalLiquidHandSoapPeppermint, // Price 1428, 2020-10-21T17:40:12.237Z
 ];
 
 // Alphabetical Expectations
@@ -52,6 +64,26 @@ const orderedByPriceDES = [
   seaMineralsBlueIrisHandSoap,
 ];
 
+const orderedByPriceASCWhenDuplicate = [
+  seaMineralsBlueIrisHandSoap,
+  citrusLavenderSoapBar,
+  citrusLavenderSoapBar,
+  naturalLaundryDetergentLavender,
+  naturalLiquidHandSoapPeppermint,
+  naturalLiquidHandSoapPeppermint,
+  laundryConcentratePaste,
+];
+
+const orderedByPriceDESWhenDuplicate = [
+  laundryConcentratePaste,
+  naturalLiquidHandSoapPeppermint,
+  naturalLiquidHandSoapPeppermint,
+  naturalLaundryDetergentLavender,
+  citrusLavenderSoapBar,
+  citrusLavenderSoapBar,
+  seaMineralsBlueIrisHandSoap,
+];
+
 // Newness Expectations
 const orderedByNewnessASC = [
   naturalLaundryDetergentLavender,
@@ -70,52 +102,47 @@ const orderedByNewnessDES = [
 
 describe('sortAlphabetically', () => {
   test('#1 should order by "ASCENDING"', () => {
-    const result = sortAlphabetically(randomlyOrdered, 'ASCENDING');
+    const result = sortAlphabetically(randomlyOrdered, ASCENDING);
     expect(result).toEqual(orderedAlphabeticallyASC);
   });
 
   test('#2 should order by "DESCENDING"', () => {
-    const result = sortAlphabetically(randomlyOrdered, 'DESCENDING');
+    const result = sortAlphabetically(randomlyOrdered, DESCENDING);
     expect(result).toEqual(orderedAlphabeticallyDES);
-  });
-
-  test('#3 should not be order by "DESCENDING"', () => {
-    const result = sortByNewness(randomlyOrdered, 'ASCENDING');
-    expect(result).not.toEqual(orderedAlphabeticallyDES);
   });
 });
 
 describe('sortByNewness', () => {
   test('#1 should order by "ASCENDING"', () => {
-    const result = sortByNewness(randomlyOrdered, 'ASCENDING');
+    const result = sortByNewness(randomlyOrdered, ASCENDING);
     expect(result).toEqual(orderedByNewnessASC);
   });
 
   test('#2 should order by "DESCENDING"', () => {
-    const result = sortByNewness(randomlyOrdered, 'DESCENDING');
+    const result = sortByNewness(randomlyOrdered, DESCENDING);
     expect(result).toEqual(orderedByNewnessDES);
-  });
-
-  test('#3 should not be order by "DESCENDING"', () => {
-    const result = sortByNewness(randomlyOrdered, 'ASCENDING');
-    expect(result).not.toEqual(orderedByNewnessDES);
   });
 });
 
 describe('sortByPrice', () => {
   test('#1 should order by "ASCENDING"', () => {
-    const result = sortByPrice(randomlyOrdered, 'ASCENDING');
+    const result = sortByPrice(randomlyOrdered, ASCENDING);
 
     expect(result).toEqual(orderedByPriceASC);
   });
 
   test('#2 should order by "DESCENDING"', () => {
-    const result = sortByPrice(randomlyOrdered, 'DESCENDING');
+    const result = sortByPrice(randomlyOrdered, DESCENDING);
     expect(result).toEqual(orderedByPriceDES);
   });
 
-  test('#3 should not be order by "DESCENDING"', () => {
-    const result = sortByPrice(randomlyOrdered, 'DESCENDING');
-    expect(result).not.toEqual(orderedByPriceASC);
+  test('#3 should order when two products have same price: case ASCENDING', () => {
+    const result = sortByPrice(randomlyOrderedWithDuplicates, ASCENDING);
+    expect(result).toEqual(orderedByPriceASCWhenDuplicate);
+  });
+
+  test('#4 should order when two products have same price: case DESCENDING', () => {
+    const result = sortByPrice(randomlyOrderedWithDuplicates, DESCENDING);
+    expect(result).toEqual(orderedByPriceDESWhenDuplicate);
   });
 });
--- src/common/ProductAssortment/SortAndFilterMenu/sorting-functions.js
+++ src/common/ProductAssortment/SortAndFilterMenu/sorting-functions.js
@@ -1,31 +1,34 @@
 import { sortByTimestampDes } from 'utils';
 
-export function sortAlphabetically(products, order) {
+export const ASCENDING = 'ASCENDING';
+export const DESCENDING = 'DESCENDING';
+
+export function sortAlphabetically(products, order = ASCENDING) {
   return [...products].sort((p1, p2) => {
     if (p1.name > p2.name) {
-      return order === 'ASCENDING' ? 1 : -1;
+      return order === ASCENDING ? 1 : -1;
     } else if (p1.name < p2.name) {
-      return order === 'ASCENDING' ? -1 : 1;
+      return order === ASCENDING ? -1 : 1;
     } else {
       return 0;
     }
   });
 }
 
-export function sortByNewness(products, order) {
+export function sortByNewness(products, order = ASCENDING) {
   const sortedDesOrder = sortByTimestampDes(products, 'createdAt') || [];
-  return order === 'ASCENDING'
+  return order === ASCENDING
     ? sortedDesOrder.reverse()
     : sortByTimestampDes(products, 'createdAt');
 }
 
-export function sortByPrice(products, order) {
+export function sortByPrice(products, order = ASCENDING) {
   return [...products].sort((p1, p2) => {
-    const isBigger = p1.inventory[0].price - p2.inventory[0].price;
-    if (isBigger > 0) {
-      return order === 'ASCENDING' ? 1 : -1;
-    } else if (isBigger < 0) {
-      return order === 'ASCENDING' ? -1 : 1;
+    const diff = p1.inventory[0].price - p2.inventory[0].price;
+    if (diff > 0) {
+      return order === ASCENDING ? 1 : -1;
+    } else if (diff < 0) {
+      return order === ASCENDING ? -1 : 1;
     } else {
       return 0;
     }
